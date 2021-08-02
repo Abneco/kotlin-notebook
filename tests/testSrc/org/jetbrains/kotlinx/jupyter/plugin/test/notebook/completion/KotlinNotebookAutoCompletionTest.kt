6c5c816ded33c0d7bde64a8c4b90b8bf8e17a45f
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2021-08-03 02:55:03 +0300

    KTNB: Add Notebook autocompletion tests
    Space-RevId: 27e74941585afb43e51094e4c7648537a62d39ee

---
+++ tests/testSrc/org/jetbrains/kotlinx/jupyter/plugin/test/notebook/completion/KotlinNotebookAutoCompletionTest.kt
@@ -0,0 +1,45 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.kotlinx.jupyter.plugin.test.notebook.completion
+
+import org.jetbrains.kotlinx.jupyter.plugin.test.baseTestDataPath
+import com.intellij.testFramework.fixtures.CompletionAutoPopupTester
+import org.jetbrains.plugins.notebooks.jupyter.JupyterBaseTestCase
+import org.jetbrains.plugins.notebooks.jupyter.configureByJupyterFile
+
+class KotlinNotebookAutoCompletionTest : JupyterBaseTestCase() {
+    override fun getTestDataPath() = "$baseTestDataPath/notebooks/autocompletion"
+
+    fun testCommandCompletion() = doTest { tester ->
+        tester.typeWithPauses("l")
+        assertEquals(listOf("help"), lookupStrings)
+    }
+
+    fun testMagicCompletion() = doTest { tester ->
+        tester.typeWithPauses("s")
+        assertEquals(listOf("use", "useLatestDescriptors"), lookupStrings)
+    }
+
+    fun testKotlinCompletionInSameCell() = doTest { tester ->
+        tester.typeWithPauses(".")
+        assertContainsElements(lookupStrings, "and", "compareTo", "hashCode")
+    }
+
+    fun testKotlinCompletionInAnotherCell() = doTest { tester ->
+        tester.typeWithPauses(".")
+        assertEmpty(lookupStrings)
+    }
+
+    private val lookupStrings: List<String> get() = myFixture?.lookupElementStrings.orEmpty()
+
+    private fun doTest(action: (CompletionAutoPopupTester) -> Unit) {
+        myFixture.configureByJupyterFile("${getTestName(true)}.ipynb", testDataPath)
+        val completionTester = CompletionAutoPopupTester(myFixture)
+        completionTester.runWithAutoPopupEnabled {
+            action(completionTester)
+        }
+    }
+
+    override fun runInDispatchThread(): Boolean {
+        return false
+    }
+}
