a8705fc6a1756411b3c2548c7466b3fc5eb9b12d
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2021-08-03 02:54:45 +0300

    KTNB: Change the tests directory layout again
    Space-RevId: d404474ee844d9deaa7e7939b7d1afe92cde0551

--- tests/testSrc/org/jetbrains/kotlinx/jupyter/plugin/test/metaLanguage/JKTMetaCodeInsightTest.kt
+++ tests/testSrc/org/jetbrains/kotlinx/jupyter/plugin/test/metaLanguage/JKTMetaCodeInsightTest.kt
@@ -1,4 +1,5 @@
-package org.jetbrains.kotlinx.jupyter.plugin.test.lang
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.kotlinx.jupyter.plugin.test.metaLanguage
 
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
 import org.jetbrains.kotlinx.jupyter.plugin.test.baseTestDataPath
@@ -7,9 +8,9 @@ class JKTMetaCodeInsightTest : LightJavaCodeInsightFixtureTestCase() {
     /**
      * @return path to test data file directory relative to root of this module.
      */
-    override fun getTestDataPath() = "$baseTestDataPath/codeInsight"
+    override fun getTestDataPath() = "$baseTestDataPath/metaLanguage/codeInsight"
 
-    fun `test simple completion`() {
+    fun `test magics completion`() {
         myFixture.testCompletionVariants("completion1.juktm", "use", "useLatestDescriptors")
     }
 
--- tests/testSrc/org/jetbrains/kotlinx/jupyter/plugin/test/metaLanguage/JKTMetaParserTest.kt
+++ tests/testSrc/org/jetbrains/kotlinx/jupyter/plugin/test/metaLanguage/JKTMetaParserTest.kt
@@ -1,4 +1,5 @@
-package org.jetbrains.kotlinx.jupyter.plugin.test.lang
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.kotlinx.jupyter.plugin.test.metaLanguage
 
 import com.intellij.testFramework.ParsingTestCase
 import org.jetbrains.kotlinx.jupyter.plugin.lang.JKTMetaFileType
@@ -9,7 +10,7 @@ class JKTMetaParserTest : ParsingTestCase("", JKTMetaFileType.EXTENSION, JKTMeta
     /**
      * @return path to test data file directory relative to root of this module.
      */
-    override fun getTestDataPath() = "$baseTestDataPath/parsing"
+    override fun getTestDataPath() = "$baseTestDataPath/metaLanguage/parsing"
 
     override fun skipSpaces() = false
     override fun includeRanges() = true
