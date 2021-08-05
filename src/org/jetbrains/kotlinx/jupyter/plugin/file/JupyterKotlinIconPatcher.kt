af2c0bfe8b3d4251e2bbc4ba1c7f1a595ef1b58f
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2021-08-05 17:47:15 +0300

    KTNB: Improve the behavior of IconPatcher
    Space-RevId: d42df35737a1d68a0de67b1a05f813c6b1188756

--- src/org/jetbrains/kotlinx/jupyter/plugin/file/JupyterKotlinIconPatcher.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/file/JupyterKotlinIconPatcher.kt
@@ -1,23 +1,14 @@
 package org.jetbrains.kotlinx.jupyter.plugin.file
 
-import com.fasterxml.jackson.databind.node.ObjectNode
 import com.intellij.ide.FileIconPatcher
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import icons.KotlinJupyterIcons
-import org.jetbrains.plugins.notebooks.jackson
-import org.jetbrains.plugins.notebooks.jupyter.nbformat.JupyterNotebookSchemaFactory
 import javax.swing.Icon
 
 class JupyterKotlinIconPatcher : FileIconPatcher {
     override fun patchIcon(baseIcon: Icon?, virtualFile: VirtualFile?, flags: Int, project: Project?): Icon? {
-        if (virtualFile == null || virtualFile.extension != "ipynb") return baseIcon
-
-        val reader = virtualFile.inputStream.reader()
-        val json = jackson.readTree(reader) as? ObjectNode ?: return baseIcon
-        val schema = JupyterNotebookSchemaFactory.createSchema(json)
-        val language = schema.getLanguage(json)
-        if (language == "kotlin") return KotlinJupyterIcons.FileIcon
-        return baseIcon
+        if (virtualFile == null || virtualFile.extension != "ipynb" || !virtualFile.isKotlinNotebook) return baseIcon
+        return KotlinJupyterIcons.FileIcon
     }
 }
--- src/org/jetbrains/kotlinx/jupyter/plugin/file/util.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/file/util.kt
@@ -1,8 +1,45 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.kotlinx.jupyter.plugin.file
 
+import com.intellij.lang.Language
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.testFramework.LightVirtualFile
+import org.jetbrains.kotlin.idea.KotlinLanguage
 import org.jetbrains.plugins.notebooks.core.impl.file.NotebookVirtualFile
+import org.jetbrains.plugins.notebooks.jupyter.NOTEBOOK_LANGUAGE
+import org.jetbrains.plugins.notebooks.jupyter.nbformat.JupyterNotebookBase
 
-val NotebookVirtualFile.isKotlinNotebook: Boolean get() {
-    return notebook.kernelSpec?.language == "kotlin"
+val VirtualFile?.isKotlinNotebook: Boolean get() {
+    return this?.notebookLanguage === KotlinLanguage.INSTANCE
+}
+
+private val VirtualFile.notebookLanguage: Language? get(){
+    if (this is NotebookVirtualFile) {
+        return notebook.language
+    }
+
+    return if (this is LightVirtualFile) {
+        // It's copy of either notebook or origin file being modified
+        val notebookFile = originalFile as? NotebookVirtualFile
+            ?: (originalFile as? LightVirtualFile)?.originalFile as? NotebookVirtualFile
+        notebookFile?.notebook?.language
+            ?: originalFile?.getUserData(NOTEBOOK_LANGUAGE)
+            ?: getLanguageFromOriginalFile(this)
+    }
+    else {
+        // It's origin file
+        getUserData(NOTEBOOK_LANGUAGE)
+            ?: getLanguageFromOriginalFile(this)?.let { language ->
+                language.also { putUserData(NOTEBOOK_LANGUAGE, it) }
+            }
+    }
+}
+
+private fun getLanguageFromOriginalFile(file: VirtualFile): Language? {
+    return try {
+        JupyterNotebookBase(file.inputStream.reader()).language
+    }
+    catch (e: Exception) {
+        null
+    }
 }
