1e17feca0695c9786150c3a00b7ae4ce8f0ff70e
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2021-09-08 19:22:32 +0300

    KTNB, DS: Add before-execution callbacks and use them to build the project before execution
    Space-RevId: 7974a3ec20a40bafb9fc3db0861cfbe6538e77c3

---
+++ src/org/jetbrains/kotlinx/jupyter/plugin/JupyterKotlinPreExecutionSourceModifier.kt
@@ -0,0 +1,27 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.kotlinx.jupyter.plugin
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.text.StringUtil
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.plugins.notebooks.jupyter.connections.execution.PreExecutionSourceModifier
+import org.jetbrains.plugins.notebooks.jupyter.nbformat.JupyterKernelSpec
+
+class JupyterKotlinPreExecutionSourceModifier : PreExecutionSourceModifier {
+    override fun amendSource(project: Project, kernelSpec: JupyterKernelSpec, source: String): String? {
+        if (kernelSpec.language != "kotlin") return null
+
+        val compilerService = JupyterKotlinProjectArtifactsService.getInstance(project)
+        val artifacts = runBlocking { compilerService.buildProject() }
+        if (artifacts.isEmpty()) return null
+        val amendedSource = buildString {
+            for (artifact in artifacts) {
+                append("@file:DependsOn(\"")
+                append(StringUtil.escapeStringCharacters(artifact))
+                append("\")\n")
+            }
+            append(source)
+        }
+        return amendedSource
+    }
+}
