9f8f259258ad6ba2db56dc9311c1695eb6b4c3b5
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2022-03-16 10:25:58 +0100

    `JupyterKotlinBundle`: clean up
    Space-RevId: 359e792601e385cb385e4fa28b57b707e0c05f6c

--- src/org/jetbrains/kotlinx/jupyter/plugin/JupyterKotlinBundle.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/JupyterKotlinBundle.kt
@@ -1,10 +1,11 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlinx.jupyter.plugin
 
-import com.intellij.BundleBase
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.Nls
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
+import java.util.function.Supplier
 
 @NonNls
 private const val BUNDLE = "messages.JupyterKotlinBundle"
@@ -23,9 +24,11 @@ object JupyterKotlinBundle : DynamicBundle(BUNDLE) {
         @PropertyKey(resourceBundle = BUNDLE) key: String,
         @Nls defaultValue: String,
         vararg params: Any
-    ): String = BundleBase.messageOrDefault(getResourceBundle(javaClass.classLoader), key, defaultValue, *params)
+    ): String = messageOrDefault(key, defaultValue, *params)
 
     @JvmStatic
-    fun messagePointer(@PropertyKey(resourceBundle = BUNDLE) key: String,
-                       vararg params: Any): java.util.function.Supplier<@Nls String> = getLazyMessage(key, *params)
+    fun messagePointer(
+        @PropertyKey(resourceBundle = BUNDLE) key: String,
+        vararg params: Any
+    ): Supplier<@Nls String> = getLazyMessage(key, *params)
 }
