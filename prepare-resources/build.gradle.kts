aac1cf78100d6918225f269daba9d4a8be3f9e61
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2022-08-31 16:43:43 +0200

    IJI-572 cache-redirector for Gradle dependencies included in installer build
    Space-RevId: 2752b2acfce2f5f67fb10498018fc2a58b1f73d0

--- prepare-resources/build.gradle.kts
+++ prepare-resources/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 repositories {
-    mavenCentral()
+    maven("https://cache-redirector.jetbrains.com/maven-central")
     maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
 }
 
