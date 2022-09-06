8f53bf2f0f1b51700243ab4a92ff256ba5439e17
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2022-09-06 14:43:03 +0200

    KTNB-30: Fix import (and other soft keywords) highlighting
    Space-RevId: 4ab69a4ec8d5bc51bf194c3ab4d02591c2cd4701

---
+++ src/org/jetbrains/kotlinx/jupyter/plugin/editor/AbstractKotlinHighlightingVisitorAdapter.kt
@@ -0,0 +1,48 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlinx.jupyter.plugin.editor
+
+import com.intellij.codeInsight.daemon.impl.AnnotationHolderImpl
+import com.intellij.codeInsight.daemon.impl.HighlightInfo
+import com.intellij.codeInsight.daemon.impl.HighlightVisitor
+import com.intellij.codeInsight.daemon.impl.analysis.HighlightInfoHolder
+import com.intellij.lang.annotation.Annotation
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.AnnotationSession
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.impl.source.tree.injected.InjectedLanguageUtilBase
+import org.jetbrains.kotlin.idea.base.highlighting.visitor.AbstractHighlightingVisitor
+import org.jetbrains.kotlin.psi.KtFile
+
+abstract class AbstractKotlinHighlightingVisitorAdapter<T: AbstractHighlightingVisitor>(
+    private val visitorFactory: (AnnotationHolder) -> T,
+) : HighlightVisitor {
+    private var visitor: T? = null
+
+    override fun suitableForFile(file: PsiFile): Boolean {
+        return file is KtFile && InjectedLanguageUtilBase.getHighlightTokens(file) != null
+    }
+
+    override fun visit(element: PsiElement) {
+        visitor?.let { element.accept(it) }
+    }
+
+    override fun analyze(file: PsiFile, updateWholeFile: Boolean, holder: HighlightInfoHolder, action: Runnable): Boolean {
+        try {
+            val annotationHolder = object : AnnotationHolderImpl(AnnotationSession(file), false) {
+                override fun add(element: Annotation?): Boolean {
+                    if (element != null) holder.add(HighlightInfo.fromAnnotation(element))
+                    return true
+                }
+            }
+
+            annotationHolder.runAnnotatorWithContext(file) { element, annoHolder ->
+                visitor = visitorFactory(annoHolder)
+                action.run()
+            }
+            return true
+        } finally {
+            visitor = null
+        }
+    }
+}
