d957eee200b07af655892314bc44935f3771fdf3
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2022-09-12 18:50:55 +0200

    [formatter, KTNB] Don't delegate injected file formatting to a top-level file
    ^KTNB-33 Fixed
    
    Space-RevId: ea221db3d0388b477a45635bfb7b74f91cda239f

---
+++ src/org/jetbrains/kotlinx/jupyter/plugin/editor/KotlinInjectedFormattingOptionsProvider.kt
@@ -0,0 +1,19 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlinx.jupyter.plugin.editor
+
+import com.intellij.formatting.InjectedFormattingOptionsProvider
+import com.intellij.lang.injection.InjectedLanguageManager
+import com.intellij.psi.PsiFile
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.plugins.notebooks.jupyter.psi.JupyterFile
+
+class KotlinInjectedFormattingOptionsProvider : InjectedFormattingOptionsProvider {
+    override fun shouldDelegateToTopLevel(file: PsiFile): Boolean? {
+        if (file !is KtFile) return null;
+
+        val project = file.project
+        val injectedManager = InjectedLanguageManager.getInstance(project)
+        if (injectedManager.getTopLevelFile(file) is JupyterFile) return false
+        return null
+    }
+}
