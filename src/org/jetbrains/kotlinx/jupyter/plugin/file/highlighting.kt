0de69b8108c5e95bba78792e6a8cd3a946a76272
Nikolay Egorov <nikolay.egorov@jetbrains.com>
2022-10-23 19:51:56 +0300

    [KTNB] Support injectedFiles filter to skip meta-files
    Space-RevId: 9182b88292471496c6fe2b59b6c0e37f7fc8d481

--- src/org/jetbrains/kotlinx/jupyter/plugin/file/highlighting.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/file/highlighting.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlinx.jupyter.plugin.file
 
+import com.intellij.codeInsight.daemon.impl.InjectedLanguageHighlightingFilesFilterProvider
 import com.intellij.codeInsight.daemon.impl.InjectedLanguageHighlightingRangeReducer
 import com.intellij.codeInsight.daemon.impl.NotebookInjectedCodeUtility
 import com.intellij.codeInsight.daemon.impl.NotebookInjectedCodeUtility.NOTEBOOK_DOCUMENT_IGNORE_ANALYSIS_RANGE
@@ -9,7 +10,9 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiFile
+import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.plugins.notebooks.jupyter.psi.JupyterFile
+import java.util.function.Predicate
 
 
 internal class KotlinNotebookInjectedRangeReducer : InjectedLanguageHighlightingRangeReducer {
@@ -34,4 +37,20 @@ internal class KotlinNotebookInjectedRangeReducer : InjectedLanguageHighlighting
         //val cellList = (jupyterFile.children.first() as? JupyterNotebook)?.psiCellList
     }
 
+}
+
+
+internal class KotlinNotebookInjectedFilesFilterProvider: InjectedLanguageHighlightingFilesFilterProvider {
+    private val notebookCodeUtility = NotebookInjectedCodeUtility
+
+    override fun provideFilterForInjectedFiles(file: PsiFile, editor: Editor): Predicate<PsiFile>? {
+        if (!notebookCodeUtility.isLooksLikeNotebookFile(file)) return null
+        return notebookInjectedKotlinFileFilter
+    }
+
+    companion object {
+        private val notebookInjectedKotlinFileFilter = Predicate<PsiFile> { file ->
+            file is KtFile
+        }
+    }
 }
