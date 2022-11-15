458c1a045a422e6f83807fa2ba245a3ca11a7646
Nikolay Egorov <nikolay.egorov@jetbrains.com>
2022-11-15 09:26:50 +0200

    Make KotlinCallChainHintsProvider open to support hints for Kotlin Notebook
    Space-RevId: af0ea14a4208d3cf8a07f391158c6fe3d8bef59a

--- src/org/jetbrains/kotlinx/jupyter/plugin/codeinsight/typeHintsProviders.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/codeinsight/typeHintsProviders.kt
@@ -1,15 +1,30 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlinx.jupyter.plugin.codeinsight
 
+import com.intellij.codeInsight.hints.FactoryInlayHintsCollector
 import com.intellij.codeInsight.hints.ImmediateConfigurable
 import com.intellij.codeInsight.hints.InlayGroup
+import com.intellij.codeInsight.hints.InlayHintsCollector
+import com.intellij.codeInsight.hints.InlayHintsSink
 import com.intellij.codeInsight.hints.SettingsKey
+import com.intellij.lang.Language
+import com.intellij.openapi.components.service
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.project.DumbService
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiWhiteSpace
+import com.intellij.util.asSafely
 import org.jetbrains.kotlin.idea.codeInsight.hints.HintType
+import org.jetbrains.kotlin.idea.codeInsight.hints.KotlinCallChainHintsProvider
 import org.jetbrains.kotlin.idea.codeInsight.hints.KotlinLambdasHintsProvider
 import org.jetbrains.kotlin.idea.codeInsight.hints.KotlinReferencesTypeHintsProvider
 import org.jetbrains.kotlin.idea.codeInsight.hints.KotlinValuesHintsProvider
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtSafeQualifiedExpression
+import org.jetbrains.kotlin.types.KotlinType
+import org.jetbrains.plugins.notebooks.jupyter.psi.impl.JupyterPsiCellImpl
 
 
 class NotebookLambdaTypeHintsProvider: KotlinNotebookAbstractInlayTypeHintsProvider<KotlinLambdasHintsProvider.Settings>() {
@@ -83,4 +98,89 @@ class NotebookValuesHintProvider: KotlinNotebookAbstractInlayTypeHintsProvider<K
     override val description: String? = null
 
     override fun preparePreview(editor: Editor, file: PsiFile, settings: KotlinValuesHintsProvider.Settings) = Unit
+}
+
+class NotebookChainCallHintProvider : KotlinCallChainHintsProvider() {
+    private val shiftMargin = KotlinNotebookAbstractInlayTypeHintsProvider.markerShift
+
+    override val previewText: String = ""
+
+    override val description: String = "${super.description} in Kotlin Notebook"
+
+    override fun preparePreview(editor: Editor, file: PsiFile, settings: Settings) {
+        super.preparePreview(editor, file, settings)
+    }
+
+    override fun isLanguageSupported(language: Language): Boolean = KotlinNotebookAbstractInlayTypeHintsProvider.isLanguageSupported(language)
+
+    override fun getCollectorFor(file: PsiFile, editor: Editor, settings: Settings, sink: InlayHintsSink): InlayHintsCollector? {
+        val project = file.project
+        val defaultCollector = super.getCollectorFor(file, editor, settings, sink)
+
+        return object : FactoryInlayHintsCollector(editor) {
+            private val document = FileDocumentManager.getInstance().getDocument(file.virtualFile)!!
+
+            override fun collect(element: PsiElement, editor: Editor, sink: InlayHintsSink): Boolean {
+                if (file.project.service<DumbService>().isDumb) return true
+                if (element is KtFile) return defaultCollector?.collect(element, editor, sink) ?: true
+                if (element !is JupyterPsiCellImpl) return true
+                //val modificationArea = if (document.getUserData(NotebookInjectedCodeUtility.NOTEBOOK_DOCUMENT_IGNORE_ANALYSIS_RANGE) != null) {
+                //    synchronized(document) { document.getUserData(NotebookInjectedCodeUtility.NOTEBOOK_DOCUMENT_IGNORE_ANALYSIS_RANGE) }
+                //} else null
+                //
+                //if (modificationArea != null && !modificationArea.contains(element.textRange)) return true
+
+                val ktFile = tryGetInjectedKtFileIfPossibleOrProvided(element, project) as? PsiFile ?: return true
+
+                return KotlinNotebookAbstractInlayTypeHintsProvider.traverseElementsAndApplyAction(ktFile) { elem ->
+                    val topmostDotQualifiedExpression =
+                        (elem.safeAs(dotQualifiedClass) ?: elem.safeAs(KtSafeQualifiedExpression::class.java))
+                        ?.takeIf { it.getParentDotQualifiedExpression() == null }
+                        ?: return@traverseElementsAndApplyAction true
+                    val targetClass = if (elem is KtSafeQualifiedExpression) KtSafeQualifiedExpression::class.java else dotQualifiedClass
+                    data class ExpressionWithType(val expression: PsiElement, val type: KotlinType)
+
+                    val context = getTypeComputationContext(topmostDotQualifiedExpression)
+
+                    var someTypeIsUnknown = false
+                    val reversedChain =
+                        generateSequence<PsiElement>(topmostDotQualifiedExpression) {
+                            it.skipParenthesesAndPostfixOperatorsDown()?.safeAs(targetClass)?.getReceiver()
+                        }
+                            .drop(1) // Except last to avoid builder.build() which has obvious type
+                            .filter { it.nextSibling.asSafely<PsiWhiteSpace>()?.textContains('\n') == true }
+                            .map { it to it.getType(context) }
+                            .takeWhile { (_, type) -> (type != null).also { if (!it) someTypeIsUnknown = true } }
+                            .map { (expression, type) -> ExpressionWithType(expression, type!!) }
+                            .windowed(2, partialWindows = true) { it.first() to it.getOrNull(1) }
+                            .filter { (expressionWithType, prevExpressionWithType) ->
+                                if (prevExpressionWithType == null) {
+                                    // Show type for expression in call chain on the first line only if it's dot qualified
+                                    dotQualifiedClass.isInstance(expressionWithType.expression.skipParenthesesAndPostfixOperatorsDown())
+                                } else {
+                                    expressionWithType.type != prevExpressionWithType.type ||
+                                            !targetClass.isInstance(prevExpressionWithType.expression.skipParenthesesAndPostfixOperatorsDown())
+                                }
+                            }
+                            .map { it.first }
+                            .toList()
+                    if (someTypeIsUnknown) return@traverseElementsAndApplyAction true
+
+                    if (reversedChain.asSequence().distinctBy { it.type }.count() < settings.uniqueTypeCount) return@traverseElementsAndApplyAction true
+
+                    for ((expression, type) in reversedChain) {
+                        sink.addInlineElement(
+                            expression.textRange.endOffset + element.textOffset + shiftMargin,
+                            true,
+                            type.getInlayPresentation(expression, factory, file.project, context),
+                            false
+                        )
+                    }
+                    return@traverseElementsAndApplyAction true
+                }
+            }
+        }
+    }
+
+    private fun <T> Any.safeAs(clazz: Class<T>) = if (this::class.java == clazz) clazz.cast(this) else null
 }
