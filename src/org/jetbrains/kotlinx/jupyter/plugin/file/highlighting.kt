7bfe7026cf1e534e041d03b5f332001e327a5d37
Nikolay Egorov <nikolay.egorov@jetbrains.com>
2022-11-17 11:39:28 +0200

    KTNB-79: Stable error highlighting for reduced API
    ^fixes KTNB-79
    
    Space-RevId: c2a5b3e78b5b80ed61991c3b27767b5132ba16e5

--- src/org/jetbrains/kotlinx/jupyter/plugin/file/highlighting.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/file/highlighting.kt
@@ -6,10 +6,12 @@ import com.intellij.codeInsight.daemon.impl.InjectedLanguageHighlightingRangeRed
 import com.intellij.codeInsight.daemon.impl.NotebookInjectedCodeUtility
 import com.intellij.codeInsight.daemon.impl.NotebookInjectedCodeUtility.NOTEBOOK_DOCUMENT_IGNORE_ANALYSIS_RANGE
 import com.intellij.codeInsight.daemon.impl.NotebookInjectedCodeUtility.NOTEBOOK_FILE_ANALYSIS_DONE_KEY
+import com.intellij.lang.injection.InjectedLanguageManager
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiLanguageInjectionHost
 import org.jetbrains.kotlin.idea.core.script.ScriptConfigurationManager
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.plugins.notebooks.jupyter.psi.JupyterFile
@@ -26,6 +28,9 @@ internal class KotlinNotebookInjectedRangeReducer : InjectedLanguageHighlighting
         val jupyterFile = file as? JupyterFile ?: return null
         val document = FileDocumentManager.getInstance().getDocument(jupyterFile.virtualFile) ?: return null
 
+        file.getNotebookCellList()?.ensureScriptConfigurations(ScriptConfigurationManager.getInstance(file.project),
+                                                               InjectedLanguageManager.getInstance(file.project))
+
         return synchronized(document) {
             if (document.getUserData(NOTEBOOK_FILE_ANALYSIS_DONE_KEY) != null) {
                 return dummyTextChangeRange
@@ -35,6 +40,16 @@ internal class KotlinNotebookInjectedRangeReducer : InjectedLanguageHighlighting
             TextRange(it.startOffset, it.endOffset + 1)
         }
         //val cellList = (jupyterFile.children.first() as? JupyterNotebook)?.psiCellList
+    }
+
+    private fun Collection<PsiLanguageInjectionHost>?.ensureScriptConfigurations(scriptingManager: ScriptConfigurationManager, manager: InjectedLanguageManager) {
+        this?.forEach {
+            manager.getInjectedPsiFiles(it)?.firstOrNull { f -> f.first is KtFile }?.first?.let { ktFile ->
+                if (ktFile is KtFile) {
+                    scriptingManager.getConfiguration(ktFile)
+                }
+            }
+        }
     }
 
 }
