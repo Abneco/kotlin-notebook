de4867519393c1996a84349c03ac287f69ef3f07
Nikolay Egorov <nikolay.egorov@jetbrains.com>
2023-03-13 17:23:47 +0200

    [KTNB] Complete HL on startup
    Space-RevId: 40096b272a2c2dcc959eda4f88f8da9ff13144f3

--- src/org/jetbrains/kotlinx/jupyter/plugin/file/highlighting/general.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/file/highlighting/general.kt
@@ -113,10 +113,12 @@ internal class KotlinNotebookInjectedRangeReducer : InjectedLanguageHighlighting
                 } else cellChangeRange
                 document.putUserData(CompleteHighlightingRange, newCompleteRange)
             }
-            if (cellIndx == null && severalUpdates?.size == 1 && severalUpdates.first() != 0) { // converge
-                severalUpdates = setOfNotNull(cellUnderEditor.ordinal).union(severalUpdates)
-                document.putUserData(NotebookDocumentTargetRanges, severalUpdates)
-                document.putUserData(CompleteHighlightingRange, cells?.get(cellUnderEditor.ordinal)?.textRange)
+            if (cellIndx == null && severalUpdates?.size == 1) { // converge
+                if (severalUpdates.first() != 0) {
+                    severalUpdates = setOfNotNull(cellUnderEditor.ordinal).union(severalUpdates)
+                    document.putUserData(NotebookDocumentTargetRanges, severalUpdates)
+                    document.putUserData(CompleteHighlightingRange, cells?.get(cellUnderEditor.ordinal)?.textRange)
+                } else return null
             }
 
             if (highlightingQueue != null && cells != null) {
