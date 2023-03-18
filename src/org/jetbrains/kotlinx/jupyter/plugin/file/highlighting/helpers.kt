5611718c9be58faa311e4d51e98aa3d589e38352
Nikolay Egorov <nikolay.egorov@jetbrains.com>
2023-03-18 20:54:10 +0200

    [KTNB] Session reset does not clears HL queue during scripting update
    Space-RevId: afbee9253c529474d8e2e764d20ccfbedc4d528b

--- src/org/jetbrains/kotlinx/jupyter/plugin/file/highlighting/helpers.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/file/highlighting/helpers.kt
@@ -200,6 +200,7 @@ internal object NotebookHighlightingUtilityObject {
         }
         invokeAndWaitIfNeeded {
             LOG.info("Requesting restart of scripting support after session restart")
+            document.getUserData(NotebookCellsUpdatesAllowedToChange)?.compareAndSet(true, false)
             JupyterKtScriptingSupport.getInstance(project).update()
         }
         invokeLater {
