d870e8a9c254354f0a81c1d3b38c278392cd70bf
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2023-03-21 16:31:48 +0100

    [KTNB] register session listener as a project listener
    Instead of registering a listener for each editor and getting callbacks from unrelated sessions, use single project listener and get target file from the session.
    
    Space-RevId: b63497a998da4076cb2b73cd2d5f3cd9bb232bad

--- src/org/jetbrains/kotlinx/jupyter/plugin/editor/KotlinNotebookEditorFactoryListener.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/editor/KotlinNotebookEditorFactoryListener.kt
@@ -1,20 +1,12 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlinx.jupyter.plugin.editor
 
-import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.impl.EditorImpl
 import com.intellij.openapi.fileEditor.FileDocumentManager
-import org.jetbrains.kotlinx.jupyter.plugin.JupyterKotlinCellExecutionCallbackFactory
 import org.jetbrains.kotlinx.jupyter.plugin.file.isKotlinNotebook
 import org.jetbrains.kotlinx.jupyter.plugin.scripting.JupyterKtScriptingSupport
-import org.jetbrains.kotlinx.jupyter.plugin.session.isKotlinNotebookSession
-import org.jetbrains.kotlinx.jupyter.plugin.util.KotlinNotebookCodegen
 import org.jetbrains.plugins.notebooks.editor.NotebookEditorCreatedCallback
-import org.jetbrains.plugins.notebooks.jupyter.connections.execution.JupyterRuntimeService
-import org.jetbrains.plugins.notebooks.jupyter.connections.execution.core.JupyterExecutionCallbackAdapter
-import org.jetbrains.plugins.notebooks.jupyter.connections.execution.core.JupyterNotebookSession
-import org.jetbrains.plugins.notebooks.jupyter.connections.execution.message.JupyterMessage
 import org.jetbrains.plugins.notebooks.jupyter.editor.isJupyter
 
 class KotlinNotebookEditorFactoryListener : NotebookEditorCreatedCallback {
@@ -23,52 +15,9 @@ class KotlinNotebookEditorFactoryListener : NotebookEditorCreatedCallback {
             editor as EditorImpl
             val file = FileDocumentManager.getInstance().getFile(editor.document)
             if (file.isKotlinNotebook) {
-                installSessionOptionsInitializer(editor)
+                val project = editor.project ?: return
+                JupyterKtScriptingSupport.getInstance(project).update()
             }
         }
-    }
-
-    private fun installSessionOptionsInitializer(editor: EditorImpl) {
-        val project = editor.project ?: return
-        JupyterKtScriptingSupport.getInstance(project).update()
-
-        project.messageBus.connect(editor.disposable).subscribe(JupyterRuntimeService.Listener.TOPIC, object : JupyterRuntimeService.Listener {
-            override fun sessionCreated(session: JupyterNotebookSession) {
-                if (session.virtualFile?.file != editor.virtualFile) return
-                if (!session.isKotlinNotebookSession()) return
-
-                val initCode = """
-                    ${KotlinNotebookCodegen.generateSessionOptions(resolveSources = true, serializeScriptData = true)}
-                    ${KotlinNotebookCodegen.generateColorSchemeChangeCode()}
-                """.trimIndent()
-
-                val mainExecutionCallback = session.virtualFile?.let { virtualFile ->
-                    JupyterKotlinCellExecutionCallbackFactory.getInstance().createNotBoundCallback(
-                        project,
-                        virtualFile
-                    )
-                }
-
-                session.execute(
-                    initCode,
-                    onMessageCreated = {},
-                    callbacks = listOfNotNull(
-                        object : JupyterExecutionCallbackAdapter() {
-                            override fun onExecuteReply(message: JupyterMessage) {
-                                LOG.debug(
-                                    "Kotlin session has been initialized with response: ${message.json}"
-                                )
-                            }
-                        },
-                        mainExecutionCallback
-                    ),
-                    silent = true,
-                )
-            }
-        })
-    }
-
-    companion object {
-        private val LOG = logger<KotlinNotebookEditorFactoryListener>()
     }
 }
