a2ddccb297332effaf6dd5ed43e27e73e9478fe2
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2023-03-21 14:19:34 +0100

    KTNB: Fix problems with service locating of lets-plot rendering services, use better API
    KTNB-193
    ^KTNB-197 Fixed
    
    Space-RevId: 1e27575713b8057c5308403280538b644ac03b1e

---
+++ src/org/jetbrains/kotlinx/jupyter/plugin/outputs/plots/IdeaBatikGraphicsNodeRenderer.kt
@@ -0,0 +1,39 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlinx.jupyter.plugin.outputs.plots
+
+import com.intellij.util.ui.ImageUtil
+import com.intellij.util.ui.StartupUiUtil
+import jetbrains.datalore.vis.swing.BatikGraphicsNodeRenderer
+import org.jetbrains.relocated.apache.batik.ext.awt.RenderingHintsKeyExt
+import org.jetbrains.relocated.apache.batik.gvt.GraphicsNode
+import java.awt.Dimension
+import java.awt.Graphics2D
+import java.awt.Rectangle
+import java.awt.image.BufferedImage
+import java.lang.ref.WeakReference
+
+class IdeaBatikGraphicsNodeRenderer : BatikGraphicsNodeRenderer {
+    override val priority: Int
+        get() = 100
+
+    override fun paint(node: GraphicsNode, g: Graphics2D, size: Dimension) {
+        val img = getRenderedImage(node, size)
+        StartupUiUtil.drawImage(g, img)
+    }
+
+    private fun getRenderedImage(node: GraphicsNode, size: Dimension): BufferedImage {
+        val width = size.width
+        val height = size.height
+        val img = ImageUtil.createImage(width, height, BufferedImage.TYPE_INT_ARGB)
+        val g: Graphics2D = img.createGraphics().apply {
+            setRenderingHint(
+                RenderingHintsKeyExt.KEY_BUFFERED_IMAGE,
+                WeakReference<Any?>(img)
+            )
+            clip(Rectangle(0, 0, width, height))
+        }
+
+        node.paint(g)
+        return img
+    }
+}
