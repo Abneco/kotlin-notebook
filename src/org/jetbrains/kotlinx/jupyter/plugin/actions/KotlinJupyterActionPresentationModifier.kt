b19a9c26f95edae60cba3bf5d211a681f3bba401
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2023-03-27 21:21:38 +0200

    KTNB: Add registry key to disable "Create Jupyter Notebook" action in Kotlin Notebook
    Space-RevId: a21ec67b623f9e7cd5a413442bca3b013dcc22df

--- src/org/jetbrains/kotlinx/jupyter/plugin/actions/KotlinJupyterActionPresentationModifier.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/actions/KotlinJupyterActionPresentationModifier.kt
@@ -3,26 +3,43 @@ package org.jetbrains.kotlinx.jupyter.plugin.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.util.registry.Registry
 import org.jetbrains.kotlinx.jupyter.plugin.file.isKotlinNotebook
 import org.jetbrains.kotlinx.jupyter.plugin.util.getVirtualFile
+import org.jetbrains.plugins.notebooks.jupyter.actions.JupyterCreateFileAction
 import org.jetbrains.plugins.notebooks.jupyter.actions.JupyterOpenNotebookInBrowserAction
 import org.jetbrains.plugins.notebooks.jupyter.debugger.JupyterDebugAction
 import org.jetbrains.plugins.notebooks.jupyter.editor.actions.JupyterActionPresentationModifier
+import kotlin.reflect.KClass
 
 class KotlinJupyterActionPresentationModifier : JupyterActionPresentationModifier {
-    override fun modifyPresentation(action: AnAction, event: AnActionEvent) {
-        val virtualFile = event.getVirtualFile()
+    private val disableJupyterCoreActions get() = Registry.`is`("kotlin.notebook.disable.jupyter.core.actions", false)
 
-        if (!virtualFile.isKotlinNotebook) return
+    private val coreActionsToDisable: List<KClass<out AnAction>> = listOf(
+        JupyterCreateFileAction::class,
+    )
+    private val editorActionsToDisableInKotlinNotebook: List<KClass<out AnAction>> = listOf(
+        JupyterDebugAction::class,
+        JupyterOpenNotebookInBrowserAction::class,
+    )
 
-        if (action.isActionToHide()) {
-            event.presentation.isEnabledAndVisible = false
-            return
-        }
+    private val AnActionEvent.isKotlinNotebookEvent: Boolean get() = getVirtualFile().isKotlinNotebook
+    private fun AnActionEvent.disable() {
+        presentation.isEnabledAndVisible = false
+    }
+    private fun AnAction.isOneOf(actionClasses: Collection<KClass<out AnAction>>): Boolean {
+        return actionClasses.any { it.isInstance(this) }
     }
 
-    private fun AnAction.isActionToHide(): Boolean {
-        return this is JupyterDebugAction
-                || this is JupyterOpenNotebookInBrowserAction
+    override fun modifyPresentation(action: AnAction, event: AnActionEvent) {
+        if (action.isOneOf(editorActionsToDisableInKotlinNotebook)) {
+            if (event.isKotlinNotebookEvent) {
+                event.disable()
+            }
+        } else if (action.isOneOf(coreActionsToDisable)) {
+            if (disableJupyterCoreActions) {
+                event.disable()
+            }
+        }
     }
 }
