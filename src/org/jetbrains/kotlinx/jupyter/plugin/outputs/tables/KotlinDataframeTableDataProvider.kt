353a8d7035bcfdd56d93b0452503639f24e7737d
Nikita Ermolenko <nikita.ermolenko@jetbrains.com>
2023-04-10 10:21:19 +0300

    [KTNB] Draft of hierarchical tables ui
    * Add support of nested columns to GridModel
    
    * Add GridMetaColumn represents non-leaf column node in columns tree
    
    * Modify MyHeaderCellComponent to support multilevel header cells for nested columns
    
    Space-RevId: d4e1df2c59fcb7eb3a16de3ff5c66595652abdec

--- src/org/jetbrains/kotlinx/jupyter/plugin/outputs/tables/KotlinDataframeTableDataProvider.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/outputs/tables/KotlinDataframeTableDataProvider.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ArrayNode
 import com.intellij.openapi.util.registry.Registry
+import com.intellij.util.containers.JBTreeTraverser
 import com.jetbrains.python.debugger.pydev.TableCommandType
 import com.jetbrains.python.debugger.pydev.tables.CommandOutputType
 import org.jetbrains.kotlinx.jupyter.plugin.outputs.tables.KotlinDataframeParsing.columnsField
@@ -13,6 +14,7 @@ import org.jetbrains.kotlinx.jupyter.plugin.outputs.tables.KotlinDataframeParsin
 import org.jetbrains.kotlinx.jupyter.plugin.outputs.tables.KotlinDataframeParsing.nRowsField
 import org.jetbrains.kotlinx.jupyter.plugin.outputs.tables.KotlinDataframeParsing.separator
 import org.jetbrains.kotlinx.jupyter.plugin.outputs.tables.KotlinDataframeParsing.serializedDataframeField
+import org.jetbrains.plugins.notebooks.tables.ColumnTreeNode
 import org.jetbrains.plugins.notebooks.tables.DSTableBundle
 import org.jetbrains.plugins.notebooks.tables.DSTableData
 import org.jetbrains.plugins.notebooks.tables.DSTableDataException
@@ -25,7 +27,6 @@ import org.jetbrains.plugins.notebooks.tables.py.DSTableDataType
 import java.util.*
 import javax.swing.RowSorter
 import javax.swing.SortOrder
-import kotlin.collections.ArrayList
 
 
 internal val isSwingUiEnabledForKotlinDataframe: Boolean
@@ -116,15 +117,86 @@ class KotlinDataFrameProvider(private val mapper: ObjectMapper = ObjectMapper())
         val data = mapper.readTree(text)
         val rawJson = mapper.readTree(data[jsonPayloadField].asText())
 
+        val rawRows = asConcatenatedRows(rawJson[serializedDataframeField])
+        val rows = rawRows.split(separator)
+        val firstRowJson = mapper.readTree(rows[0])
+
+        val columnTreeTraverser = extractHierarchy(firstRowJson)
+        val columnNames = getLeafColumnNames(columnTreeTraverser)
+
         val nRow = rawJson[nRowsField].asInt()
         val nCol = rawJson[nColsField].asInt()
-        val columnNames = mutableListOf<String>()
-        (rawJson[columnsField] as ArrayNode).elements().forEach {
-            columnNames.add(it.asText())
-        }
+
         val dimensionsStr = DSTableBundle.message("ds.table.dimensions.info", nRow, nCol)
 
-        return DSDataFrameInfo(nRow, 0, columnNames, List(columnNames.size) { null }, dimensionsStr)
+        return DSDataFrameInfo(
+            nRow,
+            0,
+            columnNames,
+            List(columnNames.size) { null },
+            dimensionsStr,
+            columnsTreeTraverser = columnTreeTraverser
+        )
+    }
+
+    private fun extractHierarchy(row: JsonNode): JBTreeTraverser<ColumnTreeNode> {
+        val root = ColumnTreeNode("root", -1, mutableListOf())
+
+        var index = 0
+        fun extractColumnsHelper(jsonNode: JsonNode, columnsNode: ColumnTreeNode, path: List<String>) {
+            if (jsonNode.isObject) {
+                jsonNode.fields().forEach { (key, value) ->
+                    val child = ColumnTreeNode(key, index++, mutableListOf())
+                    columnsNode.children.add(child)
+                    extractColumnsHelper(value, child, path + listOf(key))
+                }
+            }
+        }
+
+        extractColumnsHelper(row, root, emptyList())
+
+        return JBTreeTraverser
+            .from<ColumnTreeNode?> { node -> node.children }
+            .withRoots(root.children)
+    }
+
+    private fun getColumnPathsToValues(traverser: JBTreeTraverser<ColumnTreeNode>): List<List<String>> {
+        val paths = mutableListOf<List<String>>()
+        val stack = mutableListOf<ColumnTreeNode>()
+
+        for (node in traverser.preOrderDfsTraversal()) {
+            while (stack.isNotEmpty() && !stack.last().isParent(node)) {
+                stack.removeLast()
+            }
+
+            stack.add(node)
+
+            if (isLeaf(node)) {
+                paths.add(stack.map { it.name })
+            }
+        }
+
+        return paths
+    }
+
+    private fun ColumnTreeNode.isParent(node: ColumnTreeNode): Boolean {
+        return children.contains(node)
+    }
+
+    private fun isLeaf(node: ColumnTreeNode): Boolean {
+        return node.children.isEmpty()
+    }
+
+    private fun getLeafColumnNames(traverser: JBTreeTraverser<ColumnTreeNode>): List<String> {
+        val leafColumnNames = mutableListOf<String>()
+
+        for (node in traverser.preOrderDfsTraversal()) {
+            if (node.children.isEmpty()) {
+                leafColumnNames.add(node.name)
+            }
+        }
+
+        return leafColumnNames
     }
 
     private fun parseDataFromKotlinDataframeOutput(id: DataId, text: String): DSTableData {
@@ -133,19 +205,32 @@ class KotlinDataFrameProvider(private val mapper: ObjectMapper = ObjectMapper())
 
         val rawRows = asConcatenatedRows(rawJson[serializedDataframeField])
         val rows = rawRows.split(separator)
-        val rowJson = mapper.readTree(rows[0])
-        val keys: MutableList<String> = ArrayList()
-        val iterator = rowJson.fieldNames()
-        iterator.forEachRemaining { e: String -> keys.add(e) }
+        val firstRowJson = mapper.readTree(rows[0])
 
-        val cols = List(keys.size) { mutableListOf<String>() }
+        val columnTreeTraverser = extractHierarchy(firstRowJson)
+        val columnPaths = getColumnPathsToValues(columnTreeTraverser)
+
+        val columnValues = List(columnPaths.size) { mutableListOf<String>() }
 
         for (row in rows) {
             val json = mapper.readTree(row)
-            keys.forEachIndexed { idx, key -> cols[idx].add(json[key]?.asText() ?: "") }
+            columnPaths.forEachIndexed { idx, path ->
+                val columnValue = extractNestedValue(json, path)
+                columnValues[idx].add(columnValue?.asText() ?: "")
+            }
         }
 
-        return DSTableData(id, cols)
+        return DSTableData(id, columnValues)
+    }
+
+    private fun extractNestedValue(node: JsonNode, keys: List<String>): JsonNode? {
+        var current = node
+
+        for (key in keys) {
+            current = current[key] ?: return null
+        }
+
+        return current
     }
 
     private fun asConcatenatedRows(text: JsonNode): String {
