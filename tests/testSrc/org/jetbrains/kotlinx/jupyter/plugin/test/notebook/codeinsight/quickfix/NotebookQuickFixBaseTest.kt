a8aa68b6f15ee3386878a6386c72a120ef432922
Nikolay Egorov <nikolay.egorov@jetbrains.com>
2023-04-14 10:50:23 +0300

    [quick fix] Support quickFix actions in injected KtFiles as well for KTNB-211
    ^KTNB-211 Fixed
    
    Space-RevId: 4ae576d50482ba30167893a750622886253151cf

---
+++ tests/testSrc/org/jetbrains/kotlinx/jupyter/plugin/test/notebook/codeinsight/quickfix/NotebookQuickFixBaseTest.kt
@@ -0,0 +1,135 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlinx.jupyter.plugin.test.notebook.codeinsight.quickfix
+
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.codeInsight.intention.IntentionActionDelegate
+import com.intellij.lang.injection.InjectedLanguageManager
+import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.command.CommandProcessor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.psi.PsiFile
+import com.intellij.rt.execution.junit.FileComparisonFailure
+import com.intellij.util.ui.UIUtil
+import junit.framework.TestCase
+import org.jetbrains.kotlin.idea.test.ConfigLibraryUtil
+import org.jetbrains.kotlinx.jupyter.plugin.test.baseTestDataPath
+import org.jetbrains.kotlinx.jupyter.plugin.test.getCells
+import org.jetbrains.kotlinx.jupyter.plugin.test.isInjectedKtFile
+import org.jetbrains.kotlinx.jupyter.plugin.test.notebook.execution.KotlinNotebookExecutionBaseTestCase
+import org.junit.ComparisonFailure
+import java.io.File
+
+
+abstract class NotebookQuickFixBaseTest : KotlinNotebookExecutionBaseTestCase() {
+    override fun getTestDataPath() = "$baseTestDataPath/notebooks/codeinsight/quickfix"
+    override fun runInDispatchThread(): Boolean {
+        return true
+    }
+
+    override fun getBasePath(): String {
+        return testDataPath
+    }
+
+    override fun getProject(): Project {
+        return myFixture.project
+    }
+
+    protected var shouldBeAvailableAfterExecution: Boolean = true
+
+    protected fun findActionWithText(name: String, strict: Boolean = false): IntentionAction? {
+        for (action in myFixture.availableIntentions) {
+            if (if (strict) { name == action.text } else action.text.startsWith(name)) {
+                return action
+            }
+        }
+        return null
+    }
+
+
+    private fun applyAction(contents: String, fileName: String? = null ) {
+        val actionHint = contents.substringAfter("// ").substringBefore("\n").split('\"').filter { it.isNotBlank() && it.isNotEmpty() }
+        assert(actionHint.size == 2) { "Action should be in format \"action\" \"true of false\" " }
+        val expectedText = actionHint[0]
+        val shouldBePresent = actionHint[1].toBoolean()
+
+        val intention = findActionWithText(expectedText)
+        if (shouldBePresent) {
+            if (intention == null) {
+                fail(
+                    "Action with text '" + expectedText + "' not found\nAvailable actions:\n" +
+                            myFixture.availableIntentions.joinToString(separator = "\n") { "// \"${it.text}\" \"true\"" })
+                return
+            }
+
+            val applyQuickFix = true
+            val stubComparisonFailure: ComparisonFailure?
+            if (applyQuickFix) {
+                stubComparisonFailure = try {
+                    myFixture.launchAction(intention)
+                    null
+                } catch (invocationFailure: ComparisonFailure) {
+                    invocationFailure
+                }
+
+                UIUtil.dispatchAllInvocationEvents()
+                UIUtil.dispatchAllInvocationEvents()
+
+                if (!shouldBeAvailableAfterExecution) {
+                    var action = findActionWithText(expectedText)
+                    action = if (action == null) null else IntentionActionDelegate.unwrap(action)
+                    assertNull(
+                        "Action '${expectedText}' (${action?.javaClass}) is still available after its invocation in test " + fileName,
+                        action
+                    )
+                }
+
+            } else {
+                stubComparisonFailure = null
+            }
+            val expectedResult = FileUtil.loadFile(File("$testDataPath/${getTestName(true)}.kt.expected"), true)
+            stubComparisonFailure?.let { throw it }
+
+            TestCase.assertEquals(expectedResult, myFixture.file.text)
+        } else {
+            assertNull("Action with text ${expectedText} is present, but should not", intention)
+        }
+    }
+
+    protected fun doKotlinQuickFixTest(ktFile: PsiFile, documentContent: String) {
+        CommandProcessor.getInstance().executeCommand(project, {
+            var fileText = ""
+            try {
+                fileText = ktFile.text
+                TestCase.assertTrue("\"<caret>\" is missing in file \"$ktFile\"", documentContent.contains("<caret>"))
+
+                val contents = StringUtil.convertLineSeparators(fileText)
+
+                applyAction(contents)
+            } catch (e: FileComparisonFailure) {
+                throw e
+            } catch (e: AssertionError) {
+                throw e
+            } finally {
+                ConfigLibraryUtil.unconfigureLibrariesByDirective(myFixture.module, fileText)
+            }
+        }, "", "")
+    }
+
+
+    protected fun doTest(cellInd: Int? = null) {
+        val notebookFile = configureExecutionTest()
+        val cells = notebookFile.getCells()
+        val neededCell = (if (cellInd != null) cells.getOrNull(cellInd) else null) ?: error("Invalid cell index provided")
+
+        val injectedFile = ReadAction.compute<PsiFile, Throwable> {
+            (InjectedLanguageManager.getInstance(project)
+                .getInjectedPsiFiles(neededCell)?.firstOrNull { it.first.containingFile.isInjectedKtFile() }?.first as? PsiFile)
+        } ?: error("No suitable KtFile found in a host")
+        val rawContent = FileUtil.loadFile(File("$testDataPath/${getTestName(true)}.ipynb"), true)
+
+        doKotlinQuickFixTest(injectedFile, rawContent)
+    }
+
+}
---
+++ tests/testSrc/org/jetbrains/kotlinx/jupyter/plugin/test/notebook/codeinsight/quickfix/extensions/NotebookQuickFixIntentionsExtensionTest.kt
@@ -0,0 +1,37 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlinx.jupyter.plugin.test.notebook.codeinsight.quickfix.extensions
+
+import org.jetbrains.kotlinx.jupyter.plugin.test.notebook.codeinsight.quickfix.NotebookQuickFixBaseTest
+import org.junit.Ignore
+import org.junit.Test
+
+class NotebookQuickFixIntentionsExtensionTest : NotebookQuickFixBaseTest() {
+    override fun getTestDataPath() = "${super.getTestDataPath()}/extensions"
+
+    @Test
+    fun testExtensionForClass() {
+        doTest(1)
+    }
+
+    @Test
+    fun testExtensionForProperty() {
+        doTest(0)
+    }
+
+    @Test
+    fun testExtensionWithHeteroArguments() {
+        doTest(1)
+    }
+
+    @Test
+    fun testExtensionWithHomoArguments() {
+        doTest(1)
+    }
+
+    @Ignore("Until unresolved KtCall is not reported")
+    @Test
+    fun testExtensionWithTypeParameter() {
+        doTest(1)
+    }
+
+}
