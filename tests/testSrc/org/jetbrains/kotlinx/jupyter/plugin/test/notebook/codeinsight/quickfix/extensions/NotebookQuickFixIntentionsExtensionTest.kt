85d2aad2f1d2d620718aabfde50c140b86f49739
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2023-04-20 11:56:21 +0200

    [kotlin] Make CreateCallableMemberFromUsageFactories as UnresolvedReferenceQuickFixFactory
    When reported diagnostic element is a reference
    
    #KTIJ-25219
    
    Space-RevId: 63b6b68b3ed4479ed2d8c0c6175eeca982c7eec3

--- tests/testSrc/org/jetbrains/kotlinx/jupyter/plugin/test/notebook/codeinsight/quickfix/extensions/NotebookQuickFixIntentionsExtensionTest.kt
+++ tests/testSrc/org/jetbrains/kotlinx/jupyter/plugin/test/notebook/codeinsight/quickfix/extensions/NotebookQuickFixIntentionsExtensionTest.kt
@@ -2,37 +2,31 @@
 package org.jetbrains.kotlinx.jupyter.plugin.test.notebook.codeinsight.quickfix.extensions
 
 import org.jetbrains.kotlinx.jupyter.plugin.test.notebook.codeinsight.quickfix.NotebookQuickFixBaseTest
-import org.junit.Ignore
 import org.junit.Test
 
 class NotebookQuickFixIntentionsExtensionTest : NotebookQuickFixBaseTest() {
     override fun getTestDataPath() = "${super.getTestDataPath()}/extensions"
 
-    @Ignore("Should be investigated in context of KTIJ-25219")
     @Test
     fun testExtensionForClass() {
         doTest(1)
     }
 
-    @Ignore("Should be investigated in context of KTIJ-25219")
     @Test
     fun testExtensionForProperty() {
         doTest(0)
     }
 
-    @Ignore("Should be investigated in context of KTIJ-25219")
     @Test
     fun testExtensionWithHeteroArguments() {
         doTest(1)
     }
 
-    @Ignore("Should be investigated in context of KTIJ-25219")
     @Test
     fun testExtensionWithHomoArguments() {
         doTest(1)
     }
 
-    @Ignore("Until unresolved KtCall is not reported")
     @Test
     fun testExtensionWithTypeParameter() {
         doTest(1)
