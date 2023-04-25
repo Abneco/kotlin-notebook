ec077d53ac9a559b0ab1b85ce4259f2a4c154639
Georgii Zorabov <georgii.zorabov@jetbrains.com>
2023-04-25 22:02:01 +0000

    georgii.zorabov/make_interval_field_non_nullable [DS-4464] fixed duplicated dependency
    [DS-4464] changed findLanguageById to direct language Instance, added dependency to markdown and kotlin.compiler.common
    
    [DS-4464] changed language fields type to non-nullable
    
    [DS-4464] changed IntervalInfo language param type to non-nullable and moved !! assertion
    
    [DS-4464] added dependency to Markdown from Jupyter and changed findLanguageById to direct MarkdownLanguage call
    
    [DS-4464] made Interval.language non-nullable
    
    Merge-request: IJ-MR-106697
    Merged-by: Georgii Zorabov <georgii.zorabov@jetbrains.com>
    
    Space-RevId: c9ed7734b855d7fcd41cc9faff264203ecb812fc

--- tests/testSrc/org/jetbrains/kotlinx/jupyter/plugin/test/notebook/notebookCellLanguageProvider/KotlinNotebookCellLanguageProviderTest.kt
+++ tests/testSrc/org/jetbrains/kotlinx/jupyter/plugin/test/notebook/notebookCellLanguageProvider/KotlinNotebookCellLanguageProviderTest.kt
@@ -1,9 +1,10 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlinx.jupyter.plugin.test.notebook.notebookCellLanguageProvider
 
-import com.intellij.lang.Language
 import com.intellij.openapi.editor.impl.EditorImpl
 import com.intellij.openapi.fileTypes.PlainTextLanguage
+import org.intellij.plugins.markdown.lang.MarkdownLanguage
+import org.jetbrains.kotlin.idea.KotlinLanguage
 import org.jetbrains.kotlinx.jupyter.plugin.test.KotlinNotebookBaseTestCase
 import org.jetbrains.kotlinx.jupyter.plugin.test.baseTestDataPath
 import org.jetbrains.plugins.notebooks.jupyter.configureByJupyterFile
@@ -21,13 +22,13 @@ class KotlinNotebookCellLanguageProviderTest : KotlinNotebookBaseTestCase() {
 
         assertCodeCells("kotlin cell, markdown cell, raw cell") {
             markers {
-                marker(NotebookCellLines.CellType.CODE, 0, 4, Language.findLanguageByID("kotlin"))
-                marker(NotebookCellLines.CellType.MARKDOWN, 16, 7, Language.findLanguageByID("Markdown"))
+                marker(NotebookCellLines.CellType.CODE, 0, 4, KotlinLanguage.INSTANCE)
+                marker(NotebookCellLines.CellType.MARKDOWN, 16, 7, MarkdownLanguage.INSTANCE)
                 marker(NotebookCellLines.CellType.RAW, 43, 8, PlainTextLanguage.INSTANCE)
             }
             intervals {
-                interval(NotebookCellLines.CellType.CODE, 0..1, Language.findLanguageByID("kotlin"))
-                interval(NotebookCellLines.CellType.MARKDOWN, 2..3, Language.findLanguageByID("Markdown"))
+                interval(NotebookCellLines.CellType.CODE, 0..1, KotlinLanguage.INSTANCE)
+                interval(NotebookCellLines.CellType.MARKDOWN, 2..3, MarkdownLanguage.INSTANCE)
                 interval(NotebookCellLines.CellType.RAW, 4..5, PlainTextLanguage.INSTANCE)
             }
         }
