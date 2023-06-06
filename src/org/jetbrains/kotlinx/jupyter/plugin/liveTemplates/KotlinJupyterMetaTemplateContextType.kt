0b30cd51b12e0f1951e0178ffb1b7cb84a70cdf1
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2023-06-06 22:59:23 +0200

    KTNB-250: Implement live template context types for Kotlin Notebook
    Space-RevId: 38c3ca0e205c753d558d49885e6feaec8b5840fc

---
+++ src/org/jetbrains/kotlinx/jupyter/plugin/liveTemplates/KotlinJupyterMetaTemplateContextType.kt
@@ -0,0 +1,14 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlinx.jupyter.plugin.liveTemplates
+
+import com.intellij.codeInsight.template.TemplateActionContext
+import com.intellij.codeInsight.template.TemplateContextType
+import org.jetbrains.kotlinx.jupyter.plugin.JupyterKotlinBundle
+import org.jetbrains.kotlinx.jupyter.plugin.lang.psi.JKTMetaPSIFile
+
+// We can show this context type to users, but it might be confusing
+object KotlinJupyterMetaTemplateContextType : TemplateContextType(JupyterKotlinBundle.message("kotlin.jupyter.template.context.type.meta")) {
+    override fun isInContext(templateActionContext: TemplateActionContext): Boolean {
+        return templateActionContext.file is JKTMetaPSIFile
+    }
+}
