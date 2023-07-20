Abneco/kotlin-notebook

commit d3237315531f489d190b654d800bb81b32446986

DS-5108 Stats for Jupyter versions
Space-RevId: 9a544ded85e6719d315dc7dfce921917a6e4099a

src/org/jetbrains/kotlinx/jupyter/plugin/session/KotlinInProcessJupyterClient.kt
@@ -6,6 +6,7 @@ import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.util.Version
 import com.intellij.openapi.vfs.VirtualFileManager
 import org.jetbrains.kotlinx.jupyter.config.notebookKernelSpec
 import org.jetbrains.kotlinx.jupyter.plugin.file.highlighting.NotebookHighlightingUtilityObject.resetSessionMetaInformation
@@ -161,4 +162,6 @@ class KotlinInProcessJupyterClient(
         sessionsByKernelId.clear()
         clientSessions.clear()
     }
+
+    override suspend fun getServerVersion(): Version? = null
 }

commit 1149a5ea1b8966148174d6319f750c8743e554f0

DS-5356 Repair Jupyter Lab managed servers by reimplementing version stats collector
The bug was introduced in d3237315531f489d190b654d800bb81b32446986: the change at JupyterNotebookProcessHandler.kt:129 was incorrect and broke just announced Jupyter Lab managed servers support. That change had been done only for making JupyterGetServerVersionTest green.

After returning the old code back and extending JupyterGetServerVersionTest with Jupyter Lab, it was found that JupyterConnectionsCollector sends deceptive stats for Jupyter Lab. The call `/api` returns the version of the Jupyter Lab server instead of the Jupyter Notebook server. Obviously, those servers are different and they shouldn't be put into a single bin. This fact led to reorganizing the collector: there left no ways to obtain the server version without authentication. So, now stats is reported only after successful authentication to the server, in contrast with the previous approach when stats had been reported as soon as the existence of a server was discovered.

Also, since there was the need to change the API of `JupyterClient.getServerVersion` anyway, it was decided to improve version stats reporting for JupyterHub. Hub is not a server by itself, it's an orchestrator for other servers. Now the statistics collector reports both the version of JupyterHub and the version of the Notebook/Lab server launched by JupyterHub.

Space-RevId: cc5e4592bb4af96c744c54f92dce9be55bde3c27

src/org/jetbrains/kotlinx/jupyter/plugin/session/KotlinInProcessJupyterClient.kt
@@ -163,5 +163,5 @@ class KotlinInProcessJupyterClient(
         clientSessions.clear()
     }
 
-    override suspend fun getServerVersion(): Version? = null
+    override suspend fun getServerVersions(): Iterable<Pair<JupyterClient.VersionKind, Version>> = emptyList()
 }