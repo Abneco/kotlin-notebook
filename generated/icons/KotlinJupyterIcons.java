0a70d5e7fd9597ba72f0e832fc3b071d0f69edef
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-08-09 11:07:29 +0300

    IDEA-326151 update hash algo for icons
    Space-RevId: bb8f3acd0314190362619afb86151270b893f082

--- generated/icons/KotlinJupyterIcons.java
+++ generated/icons/KotlinJupyterIcons.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package icons;
 
 import com.intellij.ui.IconManager;
@@ -14,7 +14,7 @@ public final class KotlinJupyterIcons {
   private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, KotlinJupyterIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x16 */ public static final @NotNull Icon FileIcon = load("icons/fileIcon.svg", 544082702, 0);
-  /** 16x16 */ public static final @NotNull Icon FileIconGreyKotlin = load("icons/fileIconGreyKotlin.svg", 238021880, 0);
-  /** 16x16 */ public static final @NotNull Icon ToolWindowIcon = load("icons/toolWindowIcon.svg", 902535006, 0);
+  /** 16x16 */ public static final @NotNull Icon FileIcon = load("icons/fileIcon.svg", -1720922823, 0);
+  /** 16x16 */ public static final @NotNull Icon FileIconGreyKotlin = load("icons/fileIconGreyKotlin.svg", -1357922230, 0);
+  /** 16x16 */ public static final @NotNull Icon ToolWindowIcon = load("icons/toolWindowIcon.svg", -1243307673, 0);
 }
