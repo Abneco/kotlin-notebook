c39e18cb905dc737a73482f0082fdeb4ac7f6bc9
Andrei <andrei.kislitsyn@jetbrains.com>
2023-10-26 05:49:53 +0000

    KTNB-403: Fix UI color scheme detection
    ^KTNB-403 Fixed
    
    
    Co-authored-by: Andrei Kislitsyn <Andrei.Kislitsyn@jetbrains.com>
    
    
    Merge-request: IJ-MR-117814
    Merged-by: Ilya Muradyan <Ilya.Muradyan@jetbrains.com>
    
    Space-RevId: 61b78ba1c59fc6b499dd75d14763aecb550e5239

--- src/org/jetbrains/kotlinx/jupyter/plugin/util/ui.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/util/ui.kt
@@ -1,9 +1,8 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlinx.jupyter.plugin.util
 
-import javax.swing.UIManager
+import com.intellij.util.ui.StartupUiUtil
 
 fun uiFeelsDark(): Boolean? {
-    val lafName = UIManager.getLookAndFeel()?.name ?: return null
-    return lafName.contains("Darcula") || lafName.contains("dark", true)
+    return StartupUiUtil.isDarkTheme
 }
