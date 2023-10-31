32f40ebe1cdb3d664f5a795f28689e8924ce29e4
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2023-10-31 19:20:07 +0100

    KTNB, minor: Fix i18n inspection
    Space-RevId: b7e4e1bd96c4b1ab3191ff9a142d95c51fd689a7

--- src/org/jetbrains/kotlinx/jupyter/plugin/settings/actions/ModuleTreeUtil.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/settings/actions/ModuleTreeUtil.kt
@@ -8,6 +8,7 @@ import com.intellij.openapi.module.ModuleGrouper
 import com.intellij.openapi.module.ModuleType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.IconLoader
+import com.intellij.openapi.util.NlsSafe
 import com.intellij.openapi.util.text.NaturalComparator
 import com.intellij.ui.CheckboxTree
 import com.intellij.ui.CheckboxTree.CheckboxTreeCellRenderer
@@ -92,7 +93,9 @@ private class ModuleCheckboxRenderer(private val moduleGrouper: ModuleGrouper) :
                 textRenderer.icon = ModuleType.get(userObject).icon.applyIf(!isEnabled) { IconLoader.getDisabledIcon(this) }
             }
             is ModuleGroup -> {
-                textRenderer.append(userObject.toString(), SimpleTextAttributes.REGULAR_ATTRIBUTES)
+                // Hardcoded literal inspection works incorrectly there, it doesn't do smartcasts
+                @NlsSafe val moduleGroupName = userObject.toString()
+                textRenderer.append(moduleGroupName, SimpleTextAttributes.REGULAR_ATTRIBUTES)
                 textRenderer.icon = PlatformIcons.CLOSED_MODULE_GROUP_ICON.applyIf(!isEnabled) { IconLoader.getDisabledIcon(this) }
             }
         }
