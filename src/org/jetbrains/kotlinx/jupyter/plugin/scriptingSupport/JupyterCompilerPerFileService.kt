d2076e50be1984e4df700274e2aef00e1fe0a957
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2023-12-11 01:12:07 +0100

    KTNB-474: Revert the removal of readlock because its absence makes HL worse. To be investigated
    Space-RevId: dce0f2eb507cc0121924f76dfbecd7ebbb299523

--- src/org/jetbrains/kotlinx/jupyter/plugin/scriptingSupport/JupyterCompilerPerFileService.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/scriptingSupport/JupyterCompilerPerFileService.kt
@@ -149,15 +149,20 @@ class JupyterCompilerPerFileService(
     val executedCellsCount: Int get() = directoryCounter.get()
 
     fun scripts(): List<Pair<VirtualFile, ScriptCompilationConfigurationWrapper>> {
-        val ktFiles = runReadAction { virtualFile.file.toPsiFile(project).getInjectedKtFiles() }
-        return ktFiles.mapNotNull { ktFile ->
-            val conf = JupyterKtScriptingSupport.getConfiguration(project, ktFile)?.valueOrNull()
-            if (conf == null || conf.dependenciesClassPath.isEmpty()) {
-                ktFile.reportAsAttachment()
-                null
-            } else {
-                ktFile.virtualFile to conf
+        return runReadAction {
+            val notebookPsiFile = virtualFile.file.toPsiFile(project)
+            val ktFiles = notebookPsiFile.getInjectedKtFiles()
+            val configurations = ktFiles.mapNotNull { ktFile ->
+                val conf = JupyterKtScriptingSupport.getConfiguration(project, ktFile)?.valueOrNull()
+                if (conf == null || conf.dependenciesClassPath.isEmpty()) {
+                    ktFile.reportAsAttachment()
+                    null
+                } else {
+                    ktFile.virtualFile to conf
+                }
             }
+
+            configurations
         }
     }
 
@@ -177,7 +182,7 @@ class JupyterCompilerPerFileService(
             "Empty script dependencies found",
             Attachment(
                 virtualFilePath,
-                runReadAction { text }.takeIf { it.isNotEmpty() } ?: "[Injected file has no text]"
+                text.takeIf { it.isNotEmpty() } ?: "[Injected file has no text]"
             )
         )
     }
