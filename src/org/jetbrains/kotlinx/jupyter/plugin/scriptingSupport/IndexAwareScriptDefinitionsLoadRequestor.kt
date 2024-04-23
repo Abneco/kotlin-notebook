7645f6b8f6148d88fbee626fb568da2f7fef0e31
Nikolay Egorov <nikolay.egorov@jetbrains.com>
2024-04-23 15:57:18 +0300

    [KTNB] Run after indexing complete, not after files scanned
    Space-RevId: 8d4f7e730a7876b79803292d32a4b68b13cf7cf3

--- src/org/jetbrains/kotlinx/jupyter/plugin/scriptingSupport/IndexAwareScriptDefinitionsLoadRequestor.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/scriptingSupport/IndexAwareScriptDefinitionsLoadRequestor.kt
@@ -1,23 +1,20 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlinx.jupyter.plugin.scriptingSupport
 
+import com.intellij.openapi.application.smartReadAction
 import com.intellij.openapi.project.Project
-import com.intellij.util.indexing.UnindexedFilesScanner
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import org.jetbrains.kotlin.idea.core.script.ScriptDefinitionsManager
 
 class IndexAwareScriptDefinitionsLoadRequestor(private val project: Project) {
-    private val coroutineScope = CoroutineScope(Dispatchers.Default)
-
     fun reloadDefinitions() {
         if (project.isDisposed || !project.isInitialized) return
 
-        if (!UnindexedFilesScanner.isProjectContentFullyScanned(project)) return
-
-        coroutineScope.async {
-            ScriptDefinitionsManager.getInstance(project).allDefinitions
+        // TODO: change to Plugin scope once ready
+        project.coroutineScope.async {
+            smartReadAction(project) {
+                ScriptDefinitionsManager.getInstance(project).allDefinitions
+            }
         }
     }
 }
