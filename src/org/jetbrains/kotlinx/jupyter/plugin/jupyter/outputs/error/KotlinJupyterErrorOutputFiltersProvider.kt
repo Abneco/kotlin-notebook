bbf47c0996fb86def233bc3e5dbe84ed3035c289
Christian Melchior <christian.melchior@jetbrains.com>
2024-06-24 20:48:33 +0000

    KTNB-245 Ad support for Kotlin Notebook stacktraces in JupyterErrorOutputConsoleView.
    This includes:
    - Adding support for the "Explain with AI" action.
    - Add a filter that enables navigation from cell line references in the stacktrace to the actual cell and line.
    - Make sure that Python and Kotlin filters are not both enabled.
    
    Space-RevId: a84c4de0d5f42a4356573d3df439a0fe92d081f3

---
+++ src/org/jetbrains/kotlinx/jupyter/plugin/jupyter/outputs/error/KotlinJupyterErrorOutputFiltersProvider.kt
@@ -0,0 +1,24 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlinx.jupyter.plugin.jupyter.outputs.error
+
+import com.intellij.execution.filters.Filter
+import org.jetbrains.plugins.notebooks.jupyter.editor.outputs.error.JupyterErrorOutputFiltersProvider
+import com.intellij.openapi.editor.impl.EditorImpl
+import org.jetbrains.kotlinx.jupyter.plugin.util.toBackedNotebookFile
+import org.jetbrains.plugins.notebooks.core.impl.file.BackedNotebookVirtualFile
+import org.jetbrains.plugins.notebooks.core.impl.file.notebook
+
+/**
+ * This class adds Kotlin Notebook-specific [Filter]s for creating custom links in the exception
+ * text sent from the kernel that is displayed in
+ * [org.jetbrains.plugins.notebooks.jupyter.editor.outputs.error.JupyterErrorOutputConsoleView]
+ */
+class KotlinJupyterErrorOutputFiltersProvider: JupyterErrorOutputFiltersProvider {
+  override fun getFilters(editor: EditorImpl, exceptionType: String, exceptionValue: String): List<Filter> {
+      val notebook: BackedNotebookVirtualFile? = editor.virtualFile.toBackedNotebookFile()
+      return when(notebook?.notebook?.language?.id) {
+          "kotlin" -> listOf(KotlinNotebookLineLinkFilter(editor))
+          else -> emptyList()
+      }
+  }
+}
---
+++ src/org/jetbrains/kotlinx/jupyter/plugin/jupyter/outputs/error/KotlinNotebookLineLinkFilter.kt
@@ -0,0 +1,29 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlinx.jupyter.plugin.jupyter.outputs.error
+
+import com.intellij.execution.filters.Filter
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.editor.impl.EditorImpl
+import com.intellij.openapi.project.DumbAware
+import org.jetbrains.plugins.notebooks.core.impl.file.notebook
+import org.jetbrains.plugins.notebooks.jupyter.nbformat.JupyterNotebookBase
+
+/**
+ * Class responsible for adding a link from a kernel stack trace to the relevant line in the
+ * notebook editor, rather than linking to the compiled class, which is an implementation detail.
+ *
+ * The compiled class will still show up in the stack trace, but it should be very easy to jump
+ * to the offending code inside the Notebook.
+ */
+internal class KotlinNotebookLineLinkFilter(val editor: EditorImpl) : Filter, DumbAware, Disposable {
+  private val backedNotebookVirtualFile = editor.notebook
+  val notebook = backedNotebookVirtualFile.notebook as JupyterNotebookBase
+
+  override fun dispose() {}
+
+  override fun applyFilter(line: String, entireLength: Int): Filter.Result? {
+    val stackLineInfo = linkifyStackLine(line, entireLength) ?: return null
+    val hyperlinkInfo = KotlinCellHyperlinkInfo(stackLineInfo.executionCount, stackLineInfo.cellLine, this)
+    return Filter.Result(stackLineInfo.highlightRange.first, stackLineInfo.highlightRange.last, hyperlinkInfo)
+  }
+}
