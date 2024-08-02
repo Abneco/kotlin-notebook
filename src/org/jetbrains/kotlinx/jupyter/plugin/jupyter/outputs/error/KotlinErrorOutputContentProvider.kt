c77a4e65b516aca66d073a490628b517b5c39c87
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2024-08-02 16:58:59 +0300

    Fix KTNB-753
    Space-RevId: 27436fc70f6a6912127901c5e51bf65db64cf3ff

--- src/org/jetbrains/kotlinx/jupyter/plugin/jupyter/outputs/error/KotlinErrorOutputContentProvider.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/jupyter/outputs/error/KotlinErrorOutputContentProvider.kt
@@ -1,15 +1,16 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlinx.jupyter.plugin.jupyter.outputs.error
 
-import org.jetbrains.kotlinx.jupyter.exceptions.ReplCompilerException
+import org.jetbrains.kotlinx.jupyter.exceptions.ReplEvalRuntimeException
 import org.jetbrains.plugins.notebooks.jupyter.editor.outputs.JupyterErrorOutputDataKey
 import org.jetbrains.plugins.notebooks.jupyter.editor.outputs.error.ErrorOutputContentProvider
 
 class KotlinErrorOutputContentProvider : ErrorOutputContentProvider {
     override fun getContent(outputDataKey: JupyterErrorOutputDataKey): List<String>? {
         return when {
-            outputDataKey.exceptionType == ReplCompilerException::class.java.name -> listOf(outputDataKey.exceptionValue)
-            else -> null
+            !outputDataKey.exceptionType.startsWith("org.jetbrains.kotlinx.jupyter") -> null
+            outputDataKey.exceptionType == ReplEvalRuntimeException::class.java.name -> null
+            else -> listOf(outputDataKey.exceptionValue)
         }
     }
 }
