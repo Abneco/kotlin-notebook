55cad9369176e4fea9bd6aebaea9e03b6bdd8a14
Nikita Pavlenko <Nikita.Pavlenko@jetbrains.com>
2024-09-18 10:03:33 +0200

    [PyCharm] PY-74366 Jupyter/RemoteDev (feat): First version of notebook output synchronization in RD. Console, image and some other outputs will be properly updated.
    Space-RevId: 283aa5b0699bad1c493cf9ef6847d025028087d5

--- plots/src/com/intellij/kotlin/jupyter/plots/LetsPlotOutputDataKey.kt
+++ plots/src/com/intellij/kotlin/jupyter/plots/LetsPlotOutputDataKey.kt
@@ -1,23 +1,25 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.kotlin.jupyter.plots
 
+import com.intellij.jupyter.core.jupyter.editor.outputs.HasExecutionCount
+import com.intellij.notebooks.visualization.outputs.statistic.NotebookOutputKeyType
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
-import com.intellij.jupyter.core.jupyter.editor.outputs.HasExecutionCount
-import com.intellij.notebooks.visualization.outputs.statistic.NotebookOutputKeyType
 
+@Serializable
 data class LetsPlotOutputDataKey(
     val spec: JsonObject,
     override val executionCount: Int?,
     val applyColorScheme: Boolean,
-): HasExecutionCount {
-    override val statisticKey = NotebookOutputKeyType.LETS_PLOT
+) : HasExecutionCount {
+    override fun getStatisticKey() = NotebookOutputKeyType.LETS_PLOT
     override fun getContentForDiffing(): Any {
         return JSON.encodeToString(spec)
     }
 
     companion object {
-        private val JSON = Json { prettyPrint = true }
+        private val JSON by lazy { Json { prettyPrint = true } }
     }
 }
