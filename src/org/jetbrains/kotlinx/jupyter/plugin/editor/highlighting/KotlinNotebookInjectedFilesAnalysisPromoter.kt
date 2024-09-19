06a160110e07351081f7057d3725e245fc20e2bc
Nikolay Egorov <nikolay.egorov@jetbrains.com>
2024-09-19 11:42:17 +0300

    KTNB-783: Support K2 highlighting for injected KtFile s in Notebook
    - set the registry flag in DEV build 'idea.kotlin.plugin.use.k2' to false by default
    
    Space-RevId: d47b9dd105e745aba2c5635e8427f84aa2fcd22c

---
+++ src/org/jetbrains/kotlinx/jupyter/plugin/editor/highlighting/KotlinNotebookInjectedFilesAnalysisPromoter.kt
@@ -0,0 +1,15 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlinx.jupyter.plugin.editor.highlighting
+
+import com.intellij.injected.editor.VirtualFileWindow
+import com.intellij.psi.FileViewProvider
+import org.jetbrains.kotlin.idea.base.analysis.KotlinIdeInjectedFilesAnalysisPromoter
+import org.jetbrains.kotlinx.jupyter.plugin.util.isKotlinNotebook
+
+
+internal class KotlinNotebookInjectedFilesAnalysisPromoter : KotlinIdeInjectedFilesAnalysisPromoter {
+    override fun shouldRunAnalysisForInjectedFile(viewProvider: FileViewProvider): Boolean {
+        val virtualFile = viewProvider.virtualFile
+        return (virtualFile as? VirtualFileWindow)?.delegate?.isKotlinNotebook == true
+    }
+}
