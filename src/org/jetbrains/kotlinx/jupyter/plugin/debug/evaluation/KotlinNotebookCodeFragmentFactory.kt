70b23848a4d20999ce33bda3a23c99574af33898
Maksim Zuev <maksim.zuev@jetbrains.com>
2024-10-02 17:36:00 +0000

    [debugger] CodeFragmentFactory can produce PsiCodeFragment
    * broaden code fragment type to be able to implement non-java evaluators
    
    
    Merge-request: IJ-MR-145790
    Merged-by: Maksim Zuev <Maksim.Zuev@jetbrains.com>
    
    Space-RevId: 096badbd7e76b2c8065867349e0839452b683ab4

--- src/org/jetbrains/kotlinx/jupyter/plugin/debug/evaluation/KotlinNotebookCodeFragmentFactory.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/debug/evaluation/KotlinNotebookCodeFragmentFactory.kt
@@ -1,7 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlinx.jupyter.plugin.debug.evaluation
 
-import com.intellij.debugger.engine.evaluation.CodeFragmentFactory
+import com.intellij.debugger.engine.JavaDebuggerCodeFragmentFactory
 import com.intellij.debugger.engine.evaluation.TextWithImports
 import com.intellij.debugger.engine.evaluation.expression.EvaluatorBuilder
 import com.intellij.notebooks.jupyter.core.jupyter.JupyterLanguage
@@ -28,32 +28,33 @@ import org.jetbrains.plugins.notebooks.psi.jupyter.psi.impl.JupyterSourceImpl
  *
  * Currently, this feature is not in use, as we don't have a Debug integration yet.
  */
-class KotlinNotebookCodeFragmentFactory : CodeFragmentFactory() {
+class KotlinNotebookCodeFragmentFactory : JavaDebuggerCodeFragmentFactory() {
     private val ktCodeFragmentFactory = KotlinK1CodeFragmentFactory()
     private val ktEvaluator = KotlinEvaluatorBuilder
 
-    override fun createCodeFragment(item: TextWithImports, context: PsiElement, project: Project): JavaCodeFragment {
-        return createPresentationCodeFragment(item, context, project)
+    override fun createPsiCodeFragmentImpl(item: TextWithImports, context: PsiElement?, project: Project): JavaCodeFragment? {
+        return createPresentationPsiCodeFragment(item, context, project)
     }
 
-    override fun createPresentationCodeFragment(item: TextWithImports, context: PsiElement, project: Project): JavaCodeFragment {
+    override fun createPresentationPsiCodeFragmentImpl(item: TextWithImports, context: PsiElement?, project: Project): JavaCodeFragment? {
+        if (context == null) return null
         val injectedManager = InjectedLanguageManager.getInstance(project)
         val cellAsParent = context.parent.parent as? JupyterPsiCell
 
         if (context.context !is JupyterSourceImpl || cellAsParent == null) {
-            return ktCodeFragmentFactory.createPresentationCodeFragment(item, context, project)
+            return ktCodeFragmentFactory.createPresentationPsiCodeFragment(item, context, project)
         }
 
         val injectedElement = cellAsParent.getInjectedKtFiles(injectedManager).firstOrNull()
         val backedNotebook = context.containingFile.virtualFile.toBackedNotebookFile()
 
         if (injectedElement == null || backedNotebook == null) {
-            return ktCodeFragmentFactory.createPresentationCodeFragment(item, null, project)
+            return ktCodeFragmentFactory.createPresentationPsiCodeFragment(item, null, project)
         }
 
         val cellOffset = cellAsParent.textOffset
         val newContext = injectedElement.findElementAt(cellOffset + 1)
-        return ktCodeFragmentFactory.createPresentationCodeFragment(item, newContext, project)
+        return ktCodeFragmentFactory.createPresentationPsiCodeFragment(item, newContext, project)
     }
 
     override fun isContextAccepted(contextElement: PsiElement?): Boolean {
