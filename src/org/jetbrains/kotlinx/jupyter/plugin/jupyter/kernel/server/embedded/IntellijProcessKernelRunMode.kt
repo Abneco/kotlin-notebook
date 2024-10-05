41adb4c09c17da3dbb61fdf4ecea513e62c2b4e2
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2024-10-05 17:00:30 +0200

    KTNB-803: Update kernel version, fix Kandy rendering in embedded mode
    Space-RevId: 30f3ea45cb87961adca82528a8af41596c0d677d

--- src/org/jetbrains/kotlinx/jupyter/plugin/jupyter/kernel/server/embedded/IntellijProcessKernelRunMode.kt
+++ src/org/jetbrains/kotlinx/jupyter/plugin/jupyter/kernel/server/embedded/IntellijProcessKernelRunMode.kt
@@ -3,14 +3,38 @@ package org.jetbrains.kotlinx.jupyter.plugin.jupyter.kernel.server.embedded
 
 import org.jetbrains.kotlinx.jupyter.api.AbstractKernelRunMode
 import org.jetbrains.kotlinx.jupyter.api.StreamSubstitutionType
-import org.jetbrains.kotlinx.jupyter.api.createDefaultFilteringClassLoader
+import org.jetbrains.kotlinx.jupyter.util.ClassLoadingDelegatingStrategy
+import org.jetbrains.kotlinx.jupyter.util.DelegatingClassLoader
+import org.jetbrains.kotlinx.jupyter.util.kernelFqnPrefixes
 
 object IntellijProcessKernelRunMode : AbstractKernelRunMode("Intellij Process") {
-    override fun createIntermediaryClassLoader(parent: ClassLoader) = createDefaultFilteringClassLoader(parent)
+    override fun createIntermediaryClassLoader(parent: ClassLoader) = createIdeDelegatingClassLoader(parent)
 
     override val shouldKillProcessOnShutdown: Boolean get() = false
     override val inMemoryOutputsSupported: Boolean get() = true
     override val isRunInsideIntellijProcess: Boolean get() = true
     override val streamSubstitutionType: StreamSubstitutionType
         get() = StreamSubstitutionType.NON_BLOCKING
+}
+
+private val librariesFqnPrefixes = listOf(
+    "org.jetbrains.letsPlot."
+)
+
+private fun String.startsWithAnyOf(prefixes: List<String>): Boolean {
+    val myString = this
+    return prefixes.any { myString.startsWith(it) }
+}
+
+private fun createIdeDelegatingClassLoader(parent: ClassLoader): ClassLoader {
+    val strategy =
+        ClassLoadingDelegatingStrategy { classFqn: String, parentLoader: ClassLoader ->
+            when {
+                classFqn.startsWithAnyOf(kernelFqnPrefixes) -> parentLoader
+                classFqn.startsWithAnyOf(librariesFqnPrefixes) -> null
+                else -> parentLoader.parent
+            }
+        }
+
+    return DelegatingClassLoader(parent, strategy)
 }
