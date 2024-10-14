fa866665331c909014cc8b5e7a3c39f18b7796d6
Nikolay Egorov <nikolay.egorov@jetbrains.com>
2024-10-15 01:11:13 +0200

    KTNB-783: Unify shadowing logic for both Kotlin modes; leave note about why K1-api is still used
    Space-RevId: 87e39cecc167e3fd7c7607ab6a280159790a0a9a

--- core/src/com/intellij/kotlin/jupyter/core/editor/highlighting/visitors/KotlinPluginModeShadowingAnalyzerHandler.kt
+++ core/src/com/intellij/kotlin/jupyter/core/editor/highlighting/visitors/KotlinPluginModeShadowingAnalyzerHandler.kt
@@ -9,21 +9,35 @@ import com.intellij.kotlin.jupyter.core.ide.handlers.KotlinPluginModeAwareHandle
 import com.intellij.kotlin.jupyter.core.ide.handlers.createPluginModeAwareInstance
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.progress.ProcessCanceledException
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.analysis.api.KaSession
 import org.jetbrains.kotlin.analysis.api.analyze
 import org.jetbrains.kotlin.analysis.api.components.KaDiagnosticCheckerFilter
-import org.jetbrains.kotlin.analysis.api.diagnostics.KaDiagnosticWithPsi
 import org.jetbrains.kotlin.analysis.api.diagnostics.KaSeverity
-import org.jetbrains.kotlin.diagnostics.Errors.UNRESOLVED_REFERENCE
 import org.jetbrains.kotlin.diagnostics.Severity
 import org.jetbrains.kotlin.idea.caches.resolve.analyzeWithAllCompilerChecks
-import org.jetbrains.kotlin.idea.highlighter.AbstractKotlinHighlightVisitor.Companion.suppressHighlight
-import org.jetbrains.kotlin.idea.highlighter.AbstractKotlinHighlightVisitor.Companion.unsuppressHighlight
 import org.jetbrains.kotlin.idea.highlighter.clearAllKotlinUnresolvedReferenceKinds
-import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtFile
 
+/**
+ * This is an intermediate class to represent diagnostics data
+ * as there is no way to invoke analysis with full checks via
+ * [KaDiagnosticProvider] directly.
+ */
+data class KaDiagnosticData(
+    val psiElement: PsiElement,
+    val severity: KaSeverity,
+    val factoryName: String,
+)
+
+internal fun Severity.toKaSeverity(): KaSeverity {
+    return when (this) {
+        Severity.INFO -> KaSeverity.INFO
+        Severity.ERROR -> KaSeverity.ERROR
+        Severity.WARNING -> KaSeverity.WARNING
+    }
+}
 
 /**
  * Base class for managing analysis outside the focus cell.
@@ -33,92 +47,39 @@ import org.jetbrains.kotlin.psi.KtFile
  * All instances of this class should be stateless, ideally, objects.
  */
 sealed class KotlinPluginModeShadowingAnalyzerHandler : KotlinPluginModeAwareHandler {
+    abstract fun applyBeforeProcessingDiagnostic(diagnostic: KaDiagnosticData)
+
+    protected fun filterDiagnostic(diagnostic: KaDiagnosticData): Boolean {
+        return diagnostic.severity == KaSeverity.ERROR
+    }
+
+    protected open fun KaSession.collectDiagnostics(file: KtFile) : Collection<KaDiagnosticData> {
+        return file.collectDiagnostics(KaDiagnosticCheckerFilter.ONLY_COMMON_CHECKERS).map { psi ->
+            KaDiagnosticData(
+                psi.psi,
+                psi.severity,
+                psi.factoryName
+            )
+        }
+    }
+
+    protected fun KaSession.collectErrorDiagnostics(file: KtFile) : List<KaDiagnosticData> {
+        return collectDiagnostics(file)
+            .onEach(::applyBeforeProcessingDiagnostic)
+            .filter(::filterDiagnostic)
+    }
+
     protected fun prepareForFile(injectedFile: PsiFile) : InjectedFileHighlightingHelper {
-        val helper =  InjectedFileHighlightingHelper(injectedFile)
+        val helper = InjectedFileHighlightingHelper(injectedFile)
         helper.markTargetHost()
 
         return helper
     }
 
-    protected fun reportFailedShadowing(diagnostic: Any) {
-        LOG.warn("Cannot convert diagnostic to shadowed: $diagnostic")
-    }
-
-    abstract fun performShadowing(file: PsiFile, updateWholeFile: Boolean, holder: HighlightInfoHolder, afterAnalysis: () -> Unit = {}): Boolean
-
-    companion object {
-        internal val LOG = thisLogger()
-
-        fun create(): KotlinPluginModeShadowingAnalyzerHandler {
-            return createPluginModeAwareInstance(
-                { K1ShadowingAnalyzerHandler },
-                { K2ShadowingAnalyzerHandler }
-            )
-        }
-    }
-}
-
-object K1ShadowingAnalyzerHandler : KotlinPluginModeShadowingAnalyzerHandler() {
-    override fun performShadowing(file: PsiFile, updateWholeFile: Boolean, holder: HighlightInfoHolder, afterAnalysis: () -> Unit): Boolean {
-        if (file !is KtFile) return true
-
-        val helper = prepareForFile(file)
-        val isTargetHost = helper.isCurrentFileTarget
-
-        if (isTargetHost) {
-            file.unsuppressHighlight()
-            return true
-        }
-
-        try {
-            val seenInfos = mutableSetOf<HighlightInfo>()
-            file.analyzeWithAllCompilerChecks(
-                {
-                    if (it.severity == Severity.ERROR) {
-                        val element = it.psiElement as? KtElement
-                        element?.suppressHighlight()
-                        if (!helper.shouldAcceptDiagnostic(it.psiElement, it.factoryName)) return@analyzeWithAllCompilerChecks
-
-                        val info = convertToShadowedDeclaration(it.psiElement, it.factory.name)
-
-                        if (info != null) {
-                            seenInfos.add(info)
-                        } else reportFailedShadowing(it)
-                    }
-                }
-            )
-
-            helper.applyReceivedHighlightInfos(seenInfos, holder)
-        } catch (e: Throwable) {
-            if (e is ProcessCanceledException) {
-                throw e
-            }
-            LOG.warn("Exception during analyze", e)
-            return false
-        } finally {
-            afterAnalysis()
-        }
-
-        return true
-    }
-}
-
-
-object K2ShadowingAnalyzerHandler : KotlinPluginModeShadowingAnalyzerHandler() {
-    private fun KaSession.gatherDiagnostics(file: KtFile) :  List<KaDiagnosticWithPsi<*>> {
-        return file.collectDiagnostics(KaDiagnosticCheckerFilter.ONLY_COMMON_CHECKERS)
-            .onEach { diagnostic -> diagnostic.psi.clearAllKotlinUnresolvedReferenceKinds() }
-            // might not use strings?
-            .filter { d ->
-                d.severity == KaSeverity.ERROR &&
-                        d.factoryName == UNRESOLVED_REFERENCE.name
-            }
-    }
-
-    private fun convertToShadowed(diagnostics: List<KaDiagnosticWithPsi<*>>) : Collection<HighlightInfo> {
+    protected fun convertToShadowed(diagnostics: List<KaDiagnosticData>) : Collection<HighlightInfo> {
         return diagnostics.mapNotNull { diagnostic ->
             val info = convertToShadowedDeclaration(
-                diagnostic.psi, diagnostic.factoryName
+                diagnostic.psiElement, diagnostic.factoryName
             )
 
             if (info == null) {
@@ -128,20 +89,31 @@ object K2ShadowingAnalyzerHandler : KotlinPluginModeShadowingAnalyzerHandler() {
         }
     }
 
-    override fun performShadowing(file: PsiFile, updateWholeFile: Boolean, holder: HighlightInfoHolder, afterAnalysis: () -> Unit) : Boolean {
+    protected fun reportFailedShadowing(diagnostic: Any) {
+        LOG.warn("Cannot convert diagnostic to shadowed: $diagnostic")
+    }
+
+    open fun performShadowing(file: PsiFile, updateWholeFile: Boolean, holder: HighlightInfoHolder, afterAnalysis: () -> Unit = {}): Boolean {
         if (file !is KtFile) return true
 
         val helper = prepareForFile(file)
         val isTargetHost = helper.isCurrentFileTarget
 
         if (isTargetHost) {
+            afterAnalysis()
             return true
         }
 
         try {
             analyze(file) {
-                val diagnostics = gatherDiagnostics(file)
+                val diagnostics = collectErrorDiagnostics(file).filter {
+                    helper.shouldAcceptDiagnostic(it.psiElement, it.factoryName)
+                }
                 val seenInfos = convertToShadowed(diagnostics)
+                if (seenInfos == null) {
+                    LOG.debug("All conversion attempts failed for ${diagnostics.size} diagnostics")
+                    return true
+                }
 
                 helper.applyReceivedHighlightInfos(seenInfos, holder)
             }
@@ -156,5 +128,38 @@ object K2ShadowingAnalyzerHandler : KotlinPluginModeShadowingAnalyzerHandler() {
         }
 
         return true
+    }
+
+    companion object {
+        internal val LOG = thisLogger()
+
+        fun create(): KotlinPluginModeShadowingAnalyzerHandler {
+            return createPluginModeAwareInstance(
+                { K1ShadowingAnalyzerHandler },
+                { K2ShadowingAnalyzerHandler }
+            )
+        }
+    }
+}
+
+object K1ShadowingAnalyzerHandler : KotlinPluginModeShadowingAnalyzerHandler() {
+    override fun applyBeforeProcessingDiagnostic(diagnostic: KaDiagnosticData) = Unit
+
+    //todo:  Use of K1-specific API as there is no way to invoke with AnalysisMode == ALL_COMPILER_CHECKS from analysis api
+    override fun KaSession.collectDiagnostics(file: KtFile) : Collection<KaDiagnosticData> {
+        return file.analyzeWithAllCompilerChecks().bindingContext.diagnostics.all().map {
+            KaDiagnosticData(
+                it.psiElement,
+                it.severity.toKaSeverity(),
+                it.factory.name
+            )
+        }
+    }
+}
+
+
+object K2ShadowingAnalyzerHandler : KotlinPluginModeShadowingAnalyzerHandler() {
+    override fun applyBeforeProcessingDiagnostic(diagnostic: KaDiagnosticData) {
+        diagnostic.psiElement.clearAllKotlinUnresolvedReferenceKinds()
     }
 }
