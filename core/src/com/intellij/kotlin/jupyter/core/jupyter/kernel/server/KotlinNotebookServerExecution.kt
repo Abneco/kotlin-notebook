8a9f569c57256cf93051e1ea810bbf6bd5bdc38e
Nikita.Ashihmin <nikita.ashihmin@jetbrains.com>
2024-12-09 18:30:32 +0400

    PY-77974 Jupyter(feat): Fix copy file
    Space-RevId: 2731bb94a7bcd2be6a2a6f5a2add67f57049d05b

--- core/src/com/intellij/kotlin/jupyter/core/jupyter/kernel/server/KotlinNotebookServerExecution.kt
+++ core/src/com/intellij/kotlin/jupyter/core/jupyter/kernel/server/KotlinNotebookServerExecution.kt
@@ -15,7 +15,7 @@ import kotlinx.coroutines.Job
 import java.net.URI
 
 private val emptyKotlinConnectionParameters = JupyterConnectionParameters(
-    httpParams = JupyterHttpParams(URI.create(""), JupyterTokenAuthParams("")),
+    httpParams = JupyterHttpParams(URI.create(""), JupyterTokenAuthParams(""), authority = ""),
     serverPath = null,
     kernelName = DEFAULT_KOTLIN_KERNEL_NAME,
     configId = ""
