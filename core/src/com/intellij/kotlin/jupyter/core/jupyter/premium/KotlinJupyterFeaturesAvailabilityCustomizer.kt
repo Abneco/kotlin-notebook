5f94e402340722b7d0fd3abbd6b3e3cf31a775a1
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2025-03-24 02:36:04 +0100

    Jupyter: show milder "free-tier" editor notification in commercial products
    Space-RevId: 22c463f96606e5176d4da3bebe67424207e24264

---
+++ core/src/com/intellij/kotlin/jupyter/core/jupyter/premium/KotlinJupyterFeaturesAvailabilityCustomizer.kt
@@ -0,0 +1,13 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.kotlin.jupyter.core.jupyter.premium
+
+import com.intellij.jupyter.core.premium.JupyterFearureAvailabilityInfoProvider
+import com.intellij.jupyter.core.premium.JupyterFeatureAvailabilityContext
+import com.intellij.jupyter.core.premium.JupyterFeatureAvailabilityCustomizer
+import com.intellij.kotlin.jupyter.core.util.isKotlinNotebook
+
+class KotlinJupyterFeaturesAvailabilityCustomizer : JupyterFearureAvailabilityInfoProvider.AllFeaturesAvailable(), JupyterFeatureAvailabilityCustomizer {
+    override fun isApplicable(context: JupyterFeatureAvailabilityContext): Boolean {
+        return context.editor.isKotlinNotebook
+    }
+}
