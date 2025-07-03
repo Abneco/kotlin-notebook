93a41bd657f2ccf582e40e4e6980c74dd6e27d8c
Arkady Bazhanov <Arkadiy.Bazhanov@jetbrains.com>
2025-07-03 10:41:09 +0200

    KTNB: fix log levels for SeparateProcessKotlinKernelRunnableHandler
    Space-RevId: 0ee6c20c85e42e1d893b53fac3332e0f70dd95f2

--- core/src/com/intellij/kotlin/jupyter/core/jupyter/kernel/server/process/SeparateProcessKotlinKernelRunnableHandler.kt
+++ core/src/com/intellij/kotlin/jupyter/core/jupyter/kernel/server/process/SeparateProcessKotlinKernelRunnableHandler.kt
@@ -99,7 +99,7 @@ class SeparateProcessKotlinKernelRunnableHandler(
 
             addProcessListener(object : ProcessListener {
                 override fun onTextAvailable(event: ProcessEvent, outputType: Key<*>) {
-                    LOG.warn(event.text.trimEnd().trimStart('\r', '\n'))
+                    LOG.debug(event.text.trimEnd().trimStart('\r', '\n'))
                 }
 
                 override fun processWillTerminate(event: ProcessEvent, willBeDestroyed: Boolean) {
@@ -112,7 +112,7 @@ class SeparateProcessKotlinKernelRunnableHandler(
                 override fun processTerminated(event: ProcessEvent) {
                     if (stateMachine.terminated()) {
                         eventDispatcher.multicaster.kernelTerminated(KotlinKernelProcessEventImpl(event))
-                        LOG.warn("Kernel process terminated with code ${event.exitCode} (${event.text})")
+                        LOG.info("Kernel process terminated with code ${event.exitCode} (${event.text})")
                         LOG.warnInTests { "Destroyed Kotlin kernel ${runnableHandler.kernelId}" }
                     }
 
