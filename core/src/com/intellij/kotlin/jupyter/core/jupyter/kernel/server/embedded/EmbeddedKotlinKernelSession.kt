cf496db277ef6d803f836678bf41eab68cab5bb9
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2025-07-11 04:32:58 +0200

    KTNB: Update Kotlin kernel dependencies to 0.14.1-526
    Space-RevId: 8b98b6c4679f6c99023374e6b0115a9617345c98

--- core/src/com/intellij/kotlin/jupyter/core/jupyter/kernel/server/embedded/EmbeddedKotlinKernelSession.kt
+++ core/src/com/intellij/kotlin/jupyter/core/jupyter/kernel/server/embedded/EmbeddedKotlinKernelSession.kt
@@ -55,7 +55,6 @@ class EmbeddedKotlinKernelSession(
             DefaultResolutionInfoProviderFactory,
             loggerFactory,
             createLibraryHttpUtil(loggerFactory, IdeaHttpClient),
-            kernelConfig.ownParams.homeDir,
             kernelRunMode = IntellijProcessKernelRunMode(intellijDataProvider),
         )
 
