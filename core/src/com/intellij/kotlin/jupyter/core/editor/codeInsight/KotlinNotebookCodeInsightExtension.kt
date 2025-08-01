338755d1f4bff1461495bb640e53a834e1d055a1
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2025-08-01 13:36:45 +0200

    KTNB-953, IJ-CR-168246: Don't modify rendered types, use modified renderer instead
    Space-RevId: 6abe95f89bb1552676c116f17aa2aa8a6e2382c2

--- core/src/com/intellij/kotlin/jupyter/core/editor/codeInsight/KotlinNotebookCodeInsightExtension.kt
+++ core/src/com/intellij/kotlin/jupyter/core/editor/codeInsight/KotlinNotebookCodeInsightExtension.kt
@@ -1,36 +1,17 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.kotlin.jupyter.core.editor.codeInsight
 
-import com.intellij.kotlin.jupyter.core.debug.util.KOTLIN_NOTEBOOK_BASE_CLASS_SUFFIX
 import com.intellij.kotlin.jupyter.core.util.isInsideKotlinNotebookCodeCell
 import com.intellij.kotlin.jupyter.core.util.isKotlinNotebookCodeCell
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaDanglingFileResolutionMode
 import org.jetbrains.kotlin.idea.codeinsight.api.applicable.extensions.KotlinIdeCodeInsightExtension
 
-private const val classSuffixWithDot = "$KOTLIN_NOTEBOOK_BASE_CLASS_SUFFIX."
-private val typeNameBounds = setOf('<', '>', '.')
-
 class KotlinNotebookCodeInsightExtension: KotlinIdeCodeInsightExtension {
     override fun chooseDanglingFileResolutionMode(contextElement: PsiElement): KaDanglingFileResolutionMode? {
         if (!contextElement.isInsideKotlinNotebookCodeCell) return null
 
         return KaDanglingFileResolutionMode.IGNORE_SELF
-    }
-
-    override fun modifyRenderedType(contextElement: PsiElement, renderedType: String): String? {
-        if (!contextElement.isInsideKotlinNotebookCodeCell) return null
-
-        var resultType = renderedType
-        while (true) {
-            val suffixIndex = resultType.indexOf(classSuffixWithDot)
-            if (suffixIndex == -1) break
-            val boundIndex = resultType
-                .take(suffixIndex)
-                .indexOfLast { it in typeNameBounds }
-            resultType = resultType.removeRange(boundIndex + 1, suffixIndex + classSuffixWithDot.length)
-        }
-        return resultType
     }
 
     override fun shouldAddDeclarationBeforeUsage(container: PsiElement): Boolean? {
---
+++ core/src/com/intellij/kotlin/jupyter/core/editor/codeInsight/KotlinNotebookImplementationDetailClassNameCheckerProvider.kt
@@ -0,0 +1,23 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.kotlin.jupyter.core.editor.codeInsight
+
+import com.intellij.kotlin.jupyter.core.debug.util.KOTLIN_NOTEBOOK_BASE_CLASS_SUFFIX
+import com.intellij.kotlin.jupyter.core.editor.find.isCompiledCellClassDeclaration
+import com.intellij.kotlin.jupyter.core.util.isInsideKotlinNotebookCodeCell
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.idea.base.psi.extensions.ImplementationDetailClassNameChecker
+import org.jetbrains.kotlin.idea.base.psi.extensions.ImplementationDetailClassNameCheckerProvider
+
+class KotlinNotebookImplementationDetailClassNameCheckerProvider: ImplementationDetailClassNameCheckerProvider {
+    override fun get(contextElement: PsiElement): ImplementationDetailClassNameChecker? {
+        if (!contextElement.isInsideKotlinNotebookCodeCell && !isCompiledCellClassDeclaration(contextElement)) return null
+
+        return Checker
+    }
+
+    object Checker: ImplementationDetailClassNameChecker {
+        override fun isImplementationDetail(className: String): Boolean {
+            return className.endsWith(KOTLIN_NOTEBOOK_BASE_CLASS_SUFFIX)
+        }
+    }
+}
