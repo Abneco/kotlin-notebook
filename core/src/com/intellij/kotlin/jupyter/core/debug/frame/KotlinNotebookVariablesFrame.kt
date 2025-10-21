ab96cac91a6f4f63ee24cc6788721cc56572f771
Nikolay Egorov <nikolay.egorov@jetbrains.com>
2025-10-21 12:23:18 +0300

    KTNB-5: Prefer more structured user messages, rearrange code
    Space-RevId: d120c73619ef5e8ddd5ac0eab3ae8fa368f5ccdf

--- core/src/com/intellij/kotlin/jupyter/core/debug/frame/KotlinNotebookVariablesFrame.kt
+++ core/src/com/intellij/kotlin/jupyter/core/debug/frame/KotlinNotebookVariablesFrame.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.kotlin.jupyter.core.debug.frame
 
+import com.intellij.debugger.engine.DebugProcessImpl
 import com.intellij.debugger.engine.JavaDebuggerEvaluator
 import com.intellij.debugger.engine.JavaStackFrame
 import com.intellij.debugger.impl.PrioritizedTask
@@ -37,6 +38,38 @@ internal class KotlinNotebookVariablesFrame(
     }
     private var evaluator: XDebuggerEvaluator? = null
 
+    private fun XCompositeNode.isReadyForComputations(debugProcess: DebugProcessImpl?): Boolean {
+        return when {
+            isObsolete || debugProcess == null -> {
+                setErrorMessage(
+                    KotlinNotebookBundle.message("kotlin.jupyter.debug.node.empty.no.connection.message")
+                )
+                false
+            }
+            debugProcess.isInInitialState -> {
+                setErrorMessage(
+                    KotlinNotebookBundle.message("kotlin.jupyter.debug.node.rebuild.message")
+                )
+                false
+            }
+            !debugProcess.isAttached -> {
+                setErrorMessage(
+                    KotlinNotebookBundle.message("kotlin.jupyter.debug.node.empty.not.attached.message")
+                )
+                LOG.warnUnderDebug("Process is not attached")
+                false
+            }
+            !debugProcess.isEvaluationPossible -> {
+                showInfoMessage(
+                    KotlinNotebookBundle.message("kotlin.jupyter.debug.node.rebuild.message")
+                )
+                LOG.warn("Paused context is not yet built")
+                false
+            }
+            else -> true
+        }
+    }
+
     override fun getEqualityObject(): Any? = STACK_FRAME_EQUALITY_OBJECT
 
     // Since we focus on JVM, use Java implementations
@@ -62,25 +95,7 @@ internal class KotlinNotebookVariablesFrame(
         }
 
         val debugProcess = debugSession.debuggerSession?.process
-        if (node.isObsolete || debugProcess == null) {
-            node.setErrorMessage(
-                KotlinNotebookBundle.message("kotlin.jupyter.debug.node.empty.no.connection.message")
-            )
-            return
-        }
-        if (!debugProcess.isAttached) {
-            if (debugProcess.isInInitialState) {
-                node.setErrorMessage(
-                    KotlinNotebookBundle.message("kotlin.jupyter.debug.node.rebuild.message")
-                )
-                return
-            }
-            node.setErrorMessage(
-                KotlinNotebookBundle.message("kotlin.jupyter.debug.node.empty.not.attached.message")
-            )
-            LOG.warn("Session is not initialized")
-            return
-        }
+        if (!node.isReadyForComputations(debugProcess)) return
 
         val variablesService = KotlinNotebookSessionVariablesService.getForFile(project, debugSession.virtualFile)
         val context = debugSession.evaluationContext
@@ -102,10 +117,8 @@ internal class KotlinNotebookVariablesFrame(
 
                 val variablesState = variablesService.buildXValueListForVariablesState(virtualMachine, context)
                 if (variablesState.size() == 0) {
-                    node.setMessage(
-                        KotlinNotebookBundle.message("kotlin.jupyter.debug.node.empty.session.variables.message"),
-                        AllIcons.General.Information,
-                        SimpleTextAttributes.REGULAR_ATTRIBUTES, null
+                    node.showInfoMessage(
+                        KotlinNotebookBundle.message("kotlin.jupyter.debug.node.empty.session.variables.message")
                     )
                     return@invoke
                 }
@@ -121,6 +134,10 @@ internal class KotlinNotebookVariablesFrame(
         }
 
         super.computeChildren(node)
+    }
+
+    private fun XCompositeNode.showInfoMessage(message: String) {
+        setMessage(message, AllIcons.General.Information, SimpleTextAttributes.REGULAR_ATTRIBUTES, null)
     }
 
 }
