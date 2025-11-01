093cc11fe1e32e2feb24dc9bebfb734a7096787f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-11-01 12:05:30 +0100

    revert incorrect cleanup (merge conflicts, named params)
    Space-RevId: 29291f5ed423a1f92c4632a7526b845631fdcbc2

--- build/src/com/intellij/kotlin/jupyter/build/KotlinNotebookPluginBuildTarget.kt
+++ build/src/com/intellij/kotlin/jupyter/build/KotlinNotebookPluginBuildTarget.kt
@@ -1,9 +1,8 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.kotlin.jupyter.build
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
-import org.jetbrains.intellij.build.BuildOptions
 import org.jetbrains.intellij.build.BuildPaths.Companion.ULTIMATE_HOME
 import org.jetbrains.intellij.build.IdeaUltimateProperties
 import org.jetbrains.intellij.build.createBuildTasks
@@ -12,11 +11,12 @@ import org.jetbrains.intellij.build.impl.BuildContextImpl
 object KotlinNotebookPluginBuildTarget {
     @Suppress("RAW_RUN_BLOCKING")
     @JvmStatic
-    fun main(args: Array<String>): Unit = runBlocking(Dispatchers.Default) {
-        val options = BuildOptions().apply {
-            enableEmbeddedFrontend = false
-        }
-        val context = BuildContextImpl.createContext(ULTIMATE_HOME, IdeaUltimateProperties(ULTIMATE_HOME), options)
+    fun main(args: Array<String>) = runBlocking(Dispatchers.Default) {
+        val context = BuildContextImpl.createContext(
+            projectHome = ULTIMATE_HOME,
+            productProperties = IdeaUltimateProperties(ULTIMATE_HOME),
+        )
+        context.options.enableEmbeddedFrontend = false
 
         createBuildTasks(context).buildNonBundledPlugins(listOf(
             "intellij.notebooks.plugin",
