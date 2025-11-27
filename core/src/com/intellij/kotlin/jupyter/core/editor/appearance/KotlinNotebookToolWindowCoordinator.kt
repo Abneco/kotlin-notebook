87222e3b1f0e39363944ea59af87109ea2e75201
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-11-27 11:12:36 +0100

    IJPL-218803 IJ-MR-179029 IJ-MR-175479 IJ-MR-184126 don't export execution from lang to avoid excessive and incorrect deps on debugger
    Space-RevId: f27713c975606c010f230539bc7ffe7f9c6c815c

--- core/src/com/intellij/kotlin/jupyter/core/editor/appearance/KotlinNotebookToolWindowCoordinator.kt
+++ core/src/com/intellij/kotlin/jupyter/core/editor/appearance/KotlinNotebookToolWindowCoordinator.kt
@@ -6,13 +6,13 @@ import com.intellij.jupyter.core.core.impl.file.BackedNotebookVirtualFile
 import com.intellij.jupyter.execution.kernel.KernelProcessAttachable
 import com.intellij.jupyter.execution.toolwindow.KernelProcessToolWindowCoordinator
 import com.intellij.kotlin.jupyter.core.debug.variables.KotlinNotebookSessionVariablesService
-import com.intellij.openapi.project.Project
-import com.intellij.ui.content.Content
 import com.intellij.kotlin.jupyter.core.resources.i18n.KotlinNotebookBundle
 import com.intellij.kotlin.jupyter.core.util.toAbsolutePath
 import com.intellij.kotlin.jupyter.core.variables.NotebookVariablesToolWindowSetup
+import com.intellij.openapi.project.Project
+import com.intellij.ui.content.Content
 
-class KotlinNotebookToolWindowCoordinator(
+internal class KotlinNotebookToolWindowCoordinator(
     project: Project,
     vfile: BackedNotebookVirtualFile
 ): KernelProcessToolWindowCoordinator(project, vfile) {
