86ae05b2b27ce88aac3715dd0dfd9d21740ce5e7
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-12-11 14:57:44 +0100

    IJPL-218101 IJ-MR-181153 IJ-MR-179029 simplify BuildTarget (part 2) - introduce standalone buildPlugin
    Space-RevId: b360de8393a09b9e0f1ae1ccce0ce727cb48361c

--- build/src/com/intellij/kotlin/jupyter/build/KotlinNotebookPluginBuildTarget.kt
+++ build/src/com/intellij/kotlin/jupyter/build/KotlinNotebookPluginBuildTarget.kt
@@ -1,26 +1,17 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.kotlin.jupyter.build
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import org.jetbrains.intellij.build.BuildPaths.Companion.ULTIMATE_HOME
-import org.jetbrains.intellij.build.IdeaUltimateProperties
-import org.jetbrains.intellij.build.createBuildTasks
-import org.jetbrains.intellij.build.impl.createBuildContext
+import org.jetbrains.intellij.build.buildPlugin
 
-object KotlinNotebookPluginBuildTarget {
-    @Suppress("RAW_RUN_BLOCKING")
-    @JvmStatic
-    fun main(args: Array<String>): Unit = runBlocking(Dispatchers.Default) {
-        val context = createBuildContext(projectHome = ULTIMATE_HOME, productProperties = IdeaUltimateProperties(ULTIMATE_HOME))
-        context.options.enableEmbeddedFrontend = false
-
-        createBuildTasks(context).buildNonBundledPlugins(listOf(
-            "intellij.notebooks.plugin",
-            "intellij.jupyter.plugin",
-            "intellij.kotlin.jupyter.plugin",
-        ))
-
-        context.notifyArtifactBuilt(context.paths.artifactDir)
-    }
+internal object KotlinNotebookPluginBuildTarget {
+  @JvmStatic
+  fun main(args: Array<String>) = buildPlugin(
+    "intellij.notebooks.plugin",
+    "intellij.jupyter.plugin",
+    "intellij.kotlin.jupyter.plugin",
+  ) {
+    skipProprietaryBuildTools()
+    options { enableEmbeddedFrontend = false }
+    notifyArtifactBuilt()
+  }
 }
