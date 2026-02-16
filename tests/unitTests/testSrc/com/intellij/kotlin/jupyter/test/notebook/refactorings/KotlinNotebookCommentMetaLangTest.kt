8e9f1c551df008699aa004b3d2827e3aa7201a9d
Evgenii Ilichev <evgenii.ilichev@jetbrains.com>
2026-02-16 14:30:09 +0100

    test [KTNB-1376]: mute testLineCommentUse

--- tests/unitTests/testSrc/com/intellij/kotlin/jupyter/test/notebook/refactorings/KotlinNotebookCommentMetaLangTest.kt
+++ tests/unitTests/testSrc/com/intellij/kotlin/jupyter/test/notebook/refactorings/KotlinNotebookCommentMetaLangTest.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.kotlin.jupyter.test.notebook.refactorings
 
+import com.intellij.idea.IJIgnore
 import com.intellij.openapi.actionSystem.IdeActions
 import com.intellij.openapi.editor.CaretState
 import com.intellij.openapi.editor.LogicalPosition
@@ -10,6 +11,7 @@ import org.junit.Test
 @TestDataPath($$"$CONTENT_ROOT/testData/notebooks/refactorings/commentMetaLang")
 class KotlinNotebookCommentMetaLangTest : RefactoringTestBase(IdeActions.ACTION_COMMENT_LINE) {
 
+    @IJIgnore(issue = "KTNB-1376")
     @Test
     fun testLineCommentUse() = doTest { caretModel ->
         caretModel.setCaretsAndSelections(listOf(CaretState(LogicalPosition(1, 4), null, null)))
