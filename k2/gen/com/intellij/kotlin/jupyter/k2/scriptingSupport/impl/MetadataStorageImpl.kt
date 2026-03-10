e1969b0a029e90bf7a18ce3ecf610bc5ccb2bcb4
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2026-03-10 16:35:22 +0100

    IJPL-895 Regenerate entities in intellij.kotlin.jupyter.k2
    (cherry picked from commit 6942d6ef7f1c11b526a7af8a0c7f701dd61a1bd5)
    
    IJ-MR-195501

--- k2/gen/com/intellij/kotlin/jupyter/k2/scriptingSupport/impl/MetadataStorageImpl.kt
+++ k2/gen/com/intellij/kotlin/jupyter/k2/scriptingSupport/impl/MetadataStorageImpl.kt
@@ -10,27 +10,29 @@ import com.intellij.platform.workspace.storage.metadata.model.ValueTypeMetadata
 
 @OptIn(WorkspaceEntityInternalApi::class)
 internal object MetadataStorageImpl : MetadataStorageBase() {
-  override fun initializeMetadata() {
+    override fun initializeMetadata() {
 
-    var typeMetadata: StorageTypeMetadata
+        var typeMetadata: StorageTypeMetadata
 
-    typeMetadata =
-      FinalClassMetadata.ObjectMetadata(fqName = "com.intellij.kotlin.jupyter.k2.scriptingSupport.KotlinNotebookScriptEntitySource",
-                                        properties = listOf(OwnPropertyMetadata(isComputable = false,
-                                                                                isKey = false,
-                                                                                isOpen = false,
-                                                                                name = "virtualFileUrl",
-                                                                                valueType = ValueTypeMetadata.SimpleType.CustomType(
-                                                                                  isNullable = true,
-                                                                                  typeMetadata = FinalClassMetadata.KnownClass(fqName = "com.intellij.platform.workspace.storage.url.VirtualFileUrl")),
-                                                                                withDefault = false)),
-                                        supertypes = listOf("com.intellij.platform.workspace.storage.EntitySource"))
+        typeMetadata =
+            FinalClassMetadata.ObjectMetadata(fqName = "com.intellij.kotlin.jupyter.k2.scriptingSupport.KotlinNotebookScriptEntitySource",
+                                              properties = listOf(OwnPropertyMetadata(isComputable = false,
+                                                                                      isKey = false,
+                                                                                      isOpen = false,
+                                                                                      name = "virtualFileUrl",
+                                                                                      valueType = ValueTypeMetadata.SimpleType.CustomType(
+                                                                                          isNullable = true,
+                                                                                          typeMetadata = FinalClassMetadata.KnownClass(
+                                                                                              fqName = "com.intellij.platform.workspace.storage.url.VirtualFileUrl")),
+                                                                                      withDefault = false)),
+                                              supertypes = listOf("com.intellij.platform.workspace.storage.EntitySource"))
 
-    addMetadata(typeMetadata)
-  }
+        addMetadata(typeMetadata)
+    }
 
-  override fun initializeMetadataHash() {
-    addMetadataHash(typeFqn = "com.intellij.platform.workspace.storage.EntitySource", metadataHash = 144540208)
-    addMetadataHash(typeFqn = "com.intellij.kotlin.jupyter.k2.scriptingSupport.KotlinNotebookScriptEntitySource", metadataHash = -954209350)
-  }
+    override fun initializeMetadataHash() {
+        addMetadataHash(typeFqn = "com.intellij.platform.workspace.storage.EntitySource", metadataHash = 144540208)
+        addMetadataHash(typeFqn = "com.intellij.kotlin.jupyter.k2.scriptingSupport.KotlinNotebookScriptEntitySource",
+                        metadataHash = -954209350)
+    }
 }
