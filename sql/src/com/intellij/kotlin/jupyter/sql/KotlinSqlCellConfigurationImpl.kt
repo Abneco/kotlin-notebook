d02a81d45fd3bb78d278a9e9338d76cec1cf2b96
Nikita Pavlenko <nikita.pavlenko@jetbrains.com>
2026-07-01 17:43:31 +0200

    [PyCharm] PY-84856 Jupyter (fix): Restored SQL-cells functionality in Jupyter RemoteDev
    Functionality was broken in changes from 23.01.2026, during adding support for sql cells in kotlin notebooks.
    
    Cherry-picked from ultimate@9ff41020

--- sql/src/com/intellij/kotlin/jupyter/sql/KotlinSqlCellConfigurationImpl.kt
+++ sql/src/com/intellij/kotlin/jupyter/sql/KotlinSqlCellConfigurationImpl.kt
@@ -1,22 +1,17 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.kotlin.jupyter.sql
 
-import com.intellij.database.extractors.DataExtractor
-import com.intellij.database.psi.DbDataSource
-import com.intellij.dataspell.jupyter.sql.common.SqlCellConfiguration
-import com.intellij.jupyter.core.core.impl.file.BackedNotebookVirtualFile
-import com.intellij.jupyter.core.jupyter.connections.execution.core.JupyterExecutionCallback
+import com.intellij.dataspell.jupyter.sql.common.SqlCellCommonConfiguration
 import com.intellij.jupyter.core.jupyter.nbformat.JupyterNotebook
-import com.intellij.kotlin.jupyter.core.jupyter.execution.kotlinNotebookCellExecutionCallbackFactory
 import com.intellij.kotlin.jupyter.core.util.isKotlinNotebook
 import com.intellij.lang.LanguageNamesValidation
 import com.intellij.openapi.project.Project
 import org.jetbrains.kotlin.idea.KotlinLanguage
 
 /**
- * Configuration for SQL cells in Kotlin notebooks.
+ * Common configuration for SQL cells in Kotlin notebooks.
  */
-class KotlinSqlCellConfigurationImpl : SqlCellConfiguration {
+class KotlinSqlCellCommonConfigurationImpl : SqlCellCommonConfiguration {
 
     override fun isApplicable(notebook: JupyterNotebook): Boolean = notebook.isKotlinNotebook
 
@@ -30,22 +25,8 @@ class KotlinSqlCellConfigurationImpl : SqlCellConfiguration {
         return validator.isIdentifier(value, project)
     }
 
-    override suspend fun checkAndInstallLanguageDependencies(
-        project: Project,
-        notebookVirtualFile: BackedNotebookVirtualFile,
-    ) {
-        // There are no language dependencies for Kotlin SQL cells
-    }
-
     // https://youtrack.jetbrains.com/issue/KTNB-1340/Support-DataFrames-as-a-DataSource-in-SQL-Cells
     override fun allowDataFrameAsDataSource(): Boolean = false
 
-    override fun getExecutionCallbacks(project: Project, notebookVirtualFile: BackedNotebookVirtualFile): List<JupyterExecutionCallback> {
-        return listOf(kotlinNotebookCellExecutionCallbackFactory.createUnboundCallback(project, notebookVirtualFile))
-    }
-
     override fun getVariablePrefix(): String = "dfSql"
-
-    override fun getDataExtractor(dataSource: DbDataSource, project: Project): DataExtractor = DuckDBExtractor(project)
-
 }
